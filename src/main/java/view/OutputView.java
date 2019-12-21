a9e88b7afb4b20772a4a7bafa52d76cfc9d22064
KSKIM <kwangsoonkim@outlook.com>
2019-12-21 15:36:23 +0900

    feat: 결과를 출력하기 위한 OutputView 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,14 +1,29 @@
 package view;
 
 import domain.Menu;
+import domain.Order;
 import domain.Table;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     private static final String TOP_LINE = "┌ ─ ┐";
-    private static final String TABLE_FORMAT = "| %s |";
+    private static final String TABLE_FORMAT = "|  %s   |";
     private static final String BOTTOM_LINE = "└ ─ ┘";
+    private static final String BOTTOM_CASH_LINE = "└ $ ┘";
+
+    public static void printEndPos() {
+        System.out.println("## 포스기를 종료합니다.");
+    }
+
+    public static void printTables(final List<Table> tables, Map<Table, List<Order>> tableOrders) {
+        System.out.println("## 테이블 목록");
+        final int size = tables.size();
+        printLine(TOP_LINE, size);
+        printTableNumbers(tables);
+        printTableBottomLine(tables, tableOrders);
+    }
 
     public static void printTables(final List<Table> tables) {
         System.out.println("## 테이블 목록");
@@ -16,6 +31,10 @@ public class OutputView {
         printLine(TOP_LINE, size);
         printTableNumbers(tables);
         printLine(BOTTOM_LINE, size);
+    }
+
+    public static void printOrderCountExceed() {
+        System.out.println("최대 주문 제한 개수를 초과하여 구매할 수 없습니다.");
     }
 
     public static void printMenus(final List<Menu> menus) {
@@ -36,5 +55,21 @@ public class OutputView {
             System.out.printf(TABLE_FORMAT, table);
         }
         System.out.println();
+    }
+
+    private static void printTableBottomLine(final List<Table> tables, Map<Table, List<Order>> tableOrders) {
+        for (final Table table : tables) {
+            printTableBottomLine(tableOrders.get(table).size());
+        }
+        System.out.println();
+    }
+
+    private static void printTableBottomLine(int tableOrderCount) {
+        if (tableOrderCount == 0) {
+            System.out.print(BOTTOM_LINE);
+        }
+        if (tableOrderCount != 0) {
+            System.out.print(BOTTOM_CASH_LINE);
+        }
     }
 }
