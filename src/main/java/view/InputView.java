ee66cb90aa8a19df818de357a939d9d877e6d5e4
KSKIM <kwangsoonkim@outlook.com>
2019-12-21 15:35:34 +0900

    feat: 유저로부터 콘솔 입력을 받는 InputView 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,12 +1,55 @@
 package view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import domain.PosMenuType;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    public static PosMenuType inputPosMenu() {
+        System.out.println("## 메인화면\n1: 주문등록\n2: 결제하기\n3: 프로그램 종료\n");
+        try {
+            System.out.println("## 원하시는 기능을 선택하세요.");
+            String input = scanner.nextLine();
+            return PosMenuType.of(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println("## 잘못된 입력입니다. 다시 입력해주세요");
+            return inputPosMenu();
+        }
+    }
+
     public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
+        try {
+            System.out.println("## 주문할 테이블을 선택하세요.");
+            String input = scanner.nextLine();
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("## 잘못된 입력입니다. 다시 입력해주세요");
+            return inputTableNumber();
+        }
+    }
+
+    public static int inputMenuNumber() {
+        try {
+            System.out.println("## 등록할 메뉴를 선택하세요.");
+            String input = scanner.nextLine();
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("## 잘못된 입력입니다. 다시 입력해주세요");
+            return inputMenuNumber();
+        }
+    }
+
+    public static int inputPurchaseCount() {
+        try {
+            System.out.println("## 메뉴의 수량을 입력하세요.");
+            String input = scanner.nextLine();
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("## 잘못된 입력입니다. 다시 입력해주세요");
+            return inputPurchaseCount();
+        }
     }
 }
