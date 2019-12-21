c36b12f8e91735666f273cba8cea61480d400d27
lns13301 <rkdrbgks22@ks.ac.kr>
2019-12-21 14:30:20 +0900

    [ADD] 입력된 값의 null 처리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class InputView {
@@ -8,21 +9,45 @@ public class InputView {
 
     public static int inputTableNumber() {
         System.out.println(NEWLINE + "## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
+        int id = scanner.nextInt();
+
+        if (Objects.isNull(id)) {
+            throw new IllegalArgumentException("테이블 번호는 'null'이 될 수 없습니다.");
+        }
+
+        return id;
     }
 
     public static int inputMenu() {
         System.out.println(NEWLINE + "원하는 기능을 선택하세요.");
-        return scanner.nextInt();
+        int id = scanner.nextInt();
+
+        if (Objects.isNull(id)) {
+            throw new IllegalArgumentException("원하는 기능 번호는 'null'이 될 수 없습니다.");
+        }
+
+        return id;
     }
 
     public static int inputOrder() {
         System.out.println(NEWLINE + "등록할 메뉴를 선택하세요.");
-        return scanner.nextInt();
+        int id = scanner.nextInt();
+
+        if (Objects.isNull(id)) {
+            throw new IllegalArgumentException("메뉴는 'null'이 될 수 없습니다.");
+        }
+
+        return id;
     }
 
     public static int inputCount() {
         System.out.println(NEWLINE + "메뉴의 수량을 입력하세요.");
-        return scanner.nextInt();
+        int id = scanner.nextInt();
+
+        if (Objects.isNull(id)) {
+            throw new IllegalArgumentException("수량은 'null'이 될 수 없습니다.");
+        }
+
+        return id;
     }
 }
