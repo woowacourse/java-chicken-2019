5fc11a944ca23ac63c6f492e681c7bfc48cc0072
choiyeonseok <smr603@snu.ac.kr>
2019-12-21 17:40:06 +0900

    Refactor Basket

--- src/main/java/domain/Basket.java
+++ src/main/java/domain/Basket.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Basket {
+    private static final String NEW_LINE = "\n";
+    private static final int DISCOUNT_UNIT_COUNT = 10;
+    private static final int DISCOUNT_UNIT_MONEY = 10000;
+
     final static List<Order> orderList = new ArrayList<>();
 
     public static void addToOrderList(Menu menu, int foodCount) {
@@ -28,13 +32,13 @@ public class Basket {
     private static int discountWhenChickenCategory(int totalPrice) {
         int totalCountOfChicken = orderList.stream()
                 .mapToInt(order -> order.getFoodCountWhenChicken()).sum();
-        return totalPrice - (totalCountOfChicken / 10) * 10000;
+        return totalPrice - (totalCountOfChicken / DISCOUNT_UNIT_COUNT) * DISCOUNT_UNIT_MONEY;
     }
 
     public static String showOrderList() {
         StringBuilder sb = new StringBuilder();
         for (Order order : orderList) {
-            sb.append(order + "\n");
+            sb.append(order + NEW_LINE);
         }
         return sb.toString();
     }
