bed723663166ed5c16fe0be8a38c6c928577e5cd
MOON JINJU <mjj920930@gmail.com>
2019-12-21 15:08:35 +0900

    feat: 테이블의 주문내역을 별도의 객체로 생성

--- src/main/java/domain/order/Order.java
+++ src/main/java/domain/order/Order.java
@@ -1,4 +1,13 @@
 package domain.order;
 
+import domain.menu.Menu;
+
+import java.util.HashMap;
+
 public class Order {
+    HashMap<Menu, Amount> order = new HashMap<>();
+
+    public Order(HashMap<Menu, Amount> order) {
+        this.order = order;
+    }
 }
--- src/main/java/domain/table/Table.java
+++ src/main/java/domain/table/Table.java
@@ -1,10 +1,14 @@
 package domain.table;
 
+import domain.order.Order;
+
 public class Table {
     private final int number;
+    private Order order;
 
     public Table(final int number) {
         this.number = number;
+        this.order = new Order();
     }
 
     @Override
