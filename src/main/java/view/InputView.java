e9b2bbeb14aaa6489af6495c82689a535298b39c
gunhee lee <dlrjsgml42@naver.com>
2019-12-21 14:32:29 +0900

    [InputView] add input method about todoNumber. Store 객체의 영업중에 주문/결제/종료를 선택하는 입력이 필요하다. 이 입력값을 todoNumber라고 하는데, 이를 입력받는 메서드가 추가되었다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,4 +9,9 @@ public class InputView {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
     }
+    public static int inputTodoNumber() {
+        System.out.println("## 메인화면\n1 - 주문등록\n2 - 결제하기\n3 - 프로그램 종료\n");
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
 }
