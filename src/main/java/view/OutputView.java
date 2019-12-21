3fdcd71d66a66c89ee05bb8b63c30437f28a304e
hyochan-Kim <kom1230@hanyang.ac.kr>
2019-12-21 17:06:31 +0900

    [ADD] 결제가 필요한 테이블의 경우 표시되게끔 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,33 +8,48 @@ import java.util.List;
 public class OutputView {
     private static final String TOP_LINE = "┌ ─ ┐";
     private static final String TABLE_FORMAT = "| %s |";
-    private static final String BOTTOM_LINE = "└ ─ ┘";
+    private static final String ORIGINAL_BOTTOM_LINE = "└ ─ ┘";
+    private static final String BEFORE_PAYMENT_BOTTOM_LINE = "└ P ┘";
 
     public static void printTables(final List<Table> tables) {
-        System.out.println("## 테이블 목록");
+        System.out.println("\n## 테이블 목록");
         final int size = tables.size();
-        printLine(TOP_LINE, size);
+        printTopLine(TOP_LINE, size);
         printTableNumbers(tables);
-        printLine(BOTTOM_LINE, size);
+        printBottomLine(tables);
+        System.out.println();
     }
 
     public static void printMenus(final List<Menu> menus) {
         for (final Menu menu : menus) {
             System.out.println(menu);
         }
+        System.out.println();
     }
 
-    private static void printLine(final String line, final int count) {
+    private static void printTopLine(final String line, final int count) {
         for (int index = 0; index < count; index++) {
             System.out.print(line);
         }
-        System.out.println();
+        System.out.println("");
+    }
+
+    private static void printBottomLine(final List<Table> tables) {
+        for (final Table table : tables) {
+            if (!table.isBeforePayment()) {
+                System.out.print(BEFORE_PAYMENT_BOTTOM_LINE);
+            }
+            if (table.isBeforePayment()) {
+                System.out.print(ORIGINAL_BOTTOM_LINE);
+            }
+        }
+        System.out.println("");
     }
 
     private static void printTableNumbers(final List<Table> tables) {
         for (final Table table : tables) {
             System.out.printf(TABLE_FORMAT, table);
         }
-        System.out.println();
+        System.out.println("");
     }
 }
