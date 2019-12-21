ae4a7dab72f683f956a16081e50e7814fb4c9f17
LTTTTTE <10100aaaaa@gmail.com>
2019-12-21 13:27:58 +0900

    주문할 테이블 번호 입력받기 기능 추가 - 예외처리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,6 +7,14 @@ public class InputView {
 
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
+        return inputAsNumber();
+    }
+
+    public static int inputAsNumber() {
+        try {
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            return inputTableNumber();
+        }
     }
 }
