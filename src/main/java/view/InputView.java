7464e4f253c7070bc92fc1111a275cea60ce82fe
JunSeong <ebseud6135@naver.com>
2019-12-21 13:36:32 +0900

    입력에서 숫자가 아닌 경우 예외를 처리하고 숫자만 입력받는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,28 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
+    }
+
+    private static int inputInteger(String text) {
+        System.out.println(text);
+        try {
+            String input = scanner.nextLine();
+            validateInteger(input);
+            return Integer.parseInt(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputInteger(text);
+        }
+    }
+    private static void validateInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닙니다.");
+        }
     }
 }
