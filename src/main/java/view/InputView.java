96a47e0eabfad44aebce84189ce8b085f7482e5b
Min-92 <0201.mj.lee@gmail.com>
2019-12-21 17:00:39 +0900

    refactor : 테이블 출력 변경
    - 주문할 테이블 출력에서 테이블 출력으로 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,7 +7,7 @@ public class InputView {
   private static final Scanner scanner = new Scanner(System.in);
 
   public static int inputTableNumber() {
-    System.out.println("## 주문할 테이블을 선택하세요.");
+    System.out.println("## 테이블을 선택하세요.");
     return inputNumber();
   }
 
