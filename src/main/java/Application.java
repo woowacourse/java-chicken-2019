151243c48d64c5b5ecc4e6da30881452a2490cdc
tiroring09 <tiroring09@gmail.com>
2019-12-21 15:01:01 +0900

    [feat] 사용자가 선택한 테이블 호출

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,14 +8,26 @@ import view.OutputView;
 import java.util.List;
 
 public class Application {
-    // TODO 구현 진행
-    public static void main(String[] args) {
-        final List<Table> tables = TableRepository.tables();
-        OutputView.printTables(tables);
+	// TODO 구현 진행
+	public static void main(String[] args) {
+		final List<Table> tables = TableRepository.tables();
+		OutputView.printTables(tables); // 테이블 목록 출력
 
-        final int tableNumber = InputView.inputTableNumber();
+		final int tableNumber = InputView.inputTableNumber();
 
-        final List<Menu> menus = MenuRepository.menus();
-        OutputView.printMenus(menus);
-    }
+		final List<Menu> menus = MenuRepository.menus();
+		OutputView.printMenus(menus);
+	}
+
+	private static void findTable(List<Table> tables) {
+		final int tableNumber = InputView.inputTableNumber();
+		for (Table eachTable : tables) {
+			if (eachTable.isSame(tableNumber)) {
+				// 현재 테이블에 주문받아라
+				return; // 찾으면 끝내야 함
+			}
+			System.out.println("찾는 테이블이 없다.");
+			// 재귀
+		}
+	}
 }
