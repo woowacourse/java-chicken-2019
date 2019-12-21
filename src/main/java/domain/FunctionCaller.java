709249d153949c03bbd839e52bdb61163babcf8b
김예림 <kiel0103@naver.com>
2019-12-21 14:56:37 +0900

    기능: 입력한 기능에 맞게 해당 기능을 호출하는 기능 작성

--- src/main/java/domain/FunctionCaller.java
+++ src/main/java/domain/FunctionCaller.java
@@ -3,5 +3,17 @@ package domain;
 public class FunctionCaller {
     public static void callFunction(final int functionNumber) {
 
+        if (ProvidingFunctionRepository.isOrderRegistration(functionNumber)) {
+            new OrderRegistration().resgistOrder();
+        }
+
+        if (ProvidingFunctionRepository.isPayment(functionNumber)) {
+            new Payment().pay();
+        }
+
+        if (ProvidingFunctionRepository.isExit(functionNumber)) {
+
+        }
+
     }
 }
