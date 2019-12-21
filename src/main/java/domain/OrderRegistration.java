1fbf293445c82461f8de8f0cec72133c61cbdf25
김예림 <kiel0103@naver.com>
2019-12-21 16:35:54 +0900

    기능: 주문받기 기능 구현

--- src/main/java/domain/OrderRegistration.java
+++ src/main/java/domain/OrderRegistration.java
@@ -5,7 +5,7 @@ import view.InputView;
 import view.OutputView;
 
 public class OrderRegistration {
-    public void registerOrder() {
+    public void controlOrderRegistration() {
         final List<Table> tables = TableRepository.tables();
         OutputView.printTables(tables);
 
@@ -13,5 +13,15 @@ public class OrderRegistration {
 
         final List<Menu> menus = MenuRepository.menus();
         OutputView.printMenus(menus);
+
+        final int menu = InputView.inputMenuChoice();
+        final int numberOfMenu = InputView.inputNumberOfMenu();
+
+        registerMenu(tableNumber, menu, numberOfMenu);
+    }
+
+    private void registerMenu(final int tableNumber, final int menu, final int numberOfMenu) {
+        Order order = new Order(numberOfMenu, menu);
+        TableRepository.takeOrder(tableNumber, order);
     }
 }
