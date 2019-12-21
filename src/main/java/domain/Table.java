dfbbb782c1fd251aba2ceb29e263908894b835bf
YerinCho <forjustice9839@gmail.com>
2019-12-21 14:50:08 +0900

    add : isMax 메소드 추가

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -2,6 +2,8 @@ package domain;
 
 public class Table {
     private final int number;
+    private int chickenNumber;
+    private int drinkNumber;
 
     public Table(final int number) {
         this.number = number;
@@ -10,5 +12,12 @@ public class Table {
     @Override
     public String toString() {
         return Integer.toString(number);
+    }
+    
+    public boolean isMax() {
+	if(chickenNumber+drinkNumber>99) {
+	    return true;
+	}
+	return false;
     }
 }
