afe26760c8173e33c2197fc1feb28c0123e1464f
junho <hwanghe159@naver.com>
2019-12-21 14:17:27 +0900

    OutputView에서 chooseCommandMessage() 삭제, InputView에서 inputCommand() 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    public static int inputCommand() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
+
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
