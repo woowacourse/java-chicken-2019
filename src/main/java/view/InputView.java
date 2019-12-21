f5be20e1720caa28859b3be53bdcb7ef35102e51
barrett24 <barrett2440@gmail.com>
2019-12-21 13:36:04 +0900

    Update - 메인화면 입출력 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,23 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    public static int inputMainOption() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
+
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputAddMenu() {
+        System.out.println("## 등록할 메뉴를 선택해주세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputCountMenu() {
+        System.out.println("## 메뉴의 수량을 입력하세요.");
         return scanner.nextInt();
     }
 }
