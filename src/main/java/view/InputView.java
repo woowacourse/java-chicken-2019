a7181f8df270bbe47a4faef8fa6e159aa2be4ed4
Unknown <unknown@example.com>
2019-12-21 14:57:35 +0900

    [IMPROVE] 테이블 입력에 대한 예외처리 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
 
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
-        input = scanner.nextInt();
+        return ExceptionService.handleExceptionOfTableNumber();
     }
 
     public static int inputMenuNumber() {
