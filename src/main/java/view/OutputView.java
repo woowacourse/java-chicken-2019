c30fe759eaa4511c49c6608cd03c2c49209e6cf7
gunhee lee <dlrjsgml42@naver.com>
2019-12-21 17:49:39 +0900

    [OutputView] make logic to print ordered table. (must fix!!) 주문을 한 테이블에 ￦표시를 하는 로직을 만들었다. 그러나 작동 확인을 위해 함수가 엉성하게 짜인 면이 있다. 추후 수정할 필요가 있어 표시한다. 가능하면 이슈에 올릴텐데..

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,13 +9,14 @@ public class OutputView {
     private static final String TOP_LINE = "┌ ─ ┐";
     private static final String TABLE_FORMAT = "| %s |";
     private static final String BOTTOM_LINE = "└ ─ ┘";
+    private static final String BOTTM_LINE_ORDERED = "└ ￦ ┘";
 
     public static void printTables(final List<Table> tables) {
         System.out.println("## 테이블 목록");
         final int size = tables.size();
         printLine(TOP_LINE, size);
         printTableNumbers(tables);
-        printLine(BOTTOM_LINE, size);
+        printBottomLine(BOTTOM_LINE, BOTTM_LINE_ORDERED, tables);
     }
 
     public static void printMenus(final List<Menu> menus) {
@@ -31,6 +32,23 @@ public class OutputView {
         System.out.println();
     }
 
+    /**
+     * printBottomLine은 table의 목록을 순회하면서 만약 주문이 있다면 표시해주는 메서드이다.
+     * 현재 구현 수준이 매우 낮아, 개선이 필요하다. 값 출력되는 것 확인 후 바로 개선할 예정.
+     *
+     * @param line 일반 상황에서 그릴 라인
+     * @param orderLine 주문이 있는 테이블에 그릴 라인
+     * @param tables 테이블의 목록
+     */
+    private static void printBottomLine(final String line, final String orderLine, final List<Table> tables){
+        for (Table table : tables) {
+            if(table.isOrderEmpty()){
+                System.out.print(line);
+                continue;
+            }
+            System.out.print(orderLine);
+        }
+    }
     private static void printTableNumbers(final List<Table> tables) {
         for (final Table table : tables) {
             System.out.printf(TABLE_FORMAT, table);
