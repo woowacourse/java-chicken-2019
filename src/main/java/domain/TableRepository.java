edbbacfed9d194353adcccea1e429b14a0bca57c
HWooKim97 <rgd5454@naver.com>
2019-12-21 17:07:16 +0900

    Remove unused import

--- src/main/java/domain/TableRepository.java
+++ src/main/java/domain/TableRepository.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 public class TableRepository {
     private static final List<Table> tables = new ArrayList<>();
 
