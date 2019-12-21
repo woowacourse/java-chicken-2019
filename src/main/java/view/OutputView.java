928533adabc8fd6ac158c0edaf602e94c0ebea45
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-12-21 13:59:26 +0900

    feat: 메인화면을 출력하는 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,6 +10,13 @@ public class OutputView {
 	private static final String TABLE_FORMAT = "| %s |";
 	private static final String BOTTOM_LINE = "└ ─ ┘";
 
+	public static void printMainMenu() {
+		System.out.println("## 메인화면\n"
+			+ "1 - 주문등록\n"
+			+ "2 - 결제하기\n"
+			+ "3 - 프로그램 종료\n");
+	}
+
 	public static void printTables(final List<Table> tables) {
 		System.out.println("## 테이블 목록");
 		final int size = tables.size();
