30d3724bbc52ede912e53c6c6233b748dc257ee3
sssssseungmi <tmdal9516@naver.com>
2019-12-21 14:54:13 +0900

    Add Discount.java to calculate discounted price

---
+++ src/main/java/domain/Discount.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Discount {
+	private static final int DISCOUNT_CASH = 5;
+	
+	public void discountByCash() {
+		
+	}
+	
+	public void discountByChickenCount() {
+		
+	}
+}
