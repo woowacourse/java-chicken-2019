5e051837bb20d53130db95310fcfdcd87ba08091
Sujin <sujinemilycho@gmail.com>
2019-12-21 14:21:44 +0900

    feat: 메인화면 출력 및 실행 기능 입력 기능 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -11,11 +11,20 @@ public class Application {
     // TODO 구현 진행
     public static void main(String[] args) {
         final List<Table> tables = TableRepository.tables();
-        OutputView.printTables(tables);
-
-        final int tableNumber = InputView.inputTableNumber();
-
         final List<Menu> menus = MenuRepository.menus();
-        OutputView.printMenus(menus);
+
+        final int ORDER = 1;
+        final int PAY = 2;
+        final int PROGRAM_END = 3;
+        int function = 0;
+
+        do {
+            OutputView.printMainScreen();
+            function = InputView.inputFunctionNumber();
+
+            OutputView.printTables(tables);
+            final int tableNumber = InputView.inputTableNumber();
+            OutputView.printMenus(menus);
+        } while (function != PROGRAM_END);
     }
 }
