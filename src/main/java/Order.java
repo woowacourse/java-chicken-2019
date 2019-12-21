1e5cf59c43ede50c030cd2c5b2311460fa6649e0
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-21 15:30:37 +0900

    feat: Add getMenuChoice()
    The method gets menu valid menu input from users.

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -16,13 +16,13 @@ public class Order {
 	public Order() {
 		tables = TableRepository.tables();
 		menus = MenuRepository.menus();
-		OutputView.printMenus(menus);
 		OutputView.printTables(tables);
 	}
 
 	public void run() {
 		tableNumber =getTableNumber();
-
+		OutputView.printMenus(menus);
+		int menuChoice= getMenuChoice();
 	}
 
 	private int getTableNumber() {
@@ -30,6 +30,22 @@ public class Order {
 		try{
 			value=InputView.inputTableNumber();
 			Validation.checkPositiveRange(value,tables.size());
+		}catch(InputMismatchException e) {
+			System.out.println("숫자를 입력해주세요.");
+			InputView.flush();
+			return getTableNumber();
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+			return getTableNumber();
+		}
+		return value;
+	}
+
+	private int getMenuChoice(){
+		int value;
+		try{
+			value=InputView.inputMenuChoice();
+			Validation.checkPositiveRange(value,menus.size());
 		}catch(InputMismatchException e) {
 			System.out.println("숫자를 입력해주세요.");
 			InputView.flush();
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 
 public class InputView {
 	private static final String PROMPT_HEADER = "## ";
-	private static final String PROMPT_TABLE = "테이블을 선택하세요.";
+	private static final String PROMPT_TABLE_CHOICE = "테이블을 선택하세요.";
+	private static final String PROMPT_MENU_CHOICE = "등록할 메뉴를 선택하세요.";
 	private static final String PROMPT_MAIN_COMMAND = "원하는 기능을 선택하세요.";
 	private static final Scanner scanner = new Scanner(System.in);
 
 	public static int inputTableNumber() throws InputMismatchException {
-		System.out.println(PROMPT_HEADER + PROMPT_TABLE);
+		System.out.println(PROMPT_HEADER + PROMPT_TABLE_CHOICE);
+		return scanner.nextInt();
+	}
+
+	public static int inputMenuChoice() throws InputMismatchException{
+		System.out.println(PROMPT_HEADER+PROMPT_MENU_CHOICE);
 		return scanner.nextInt();
 	}
 
