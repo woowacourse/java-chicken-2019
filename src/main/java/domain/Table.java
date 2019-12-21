5a44b851cc1ee138826ed8b588a552f06801917e
ParkSanggi <roqkfwkehlwk@naver.com>
2019-12-21 17:23:59 +0900

    feat : 8. 테이블에 메뉴가 등록되어 있는지 확인한다.

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -2,6 +2,7 @@ package domain;
 
 public class Table {
     private final int number;
+    private Menu menu;
 
     public Table(final int number) {
         this.number = number;
@@ -10,5 +11,13 @@ public class Table {
     @Override
     public String toString() {
         return Integer.toString(number);
+    }
+
+    public boolean isThisNumber(int number) {
+        return this.number == number;
+    }
+
+    public boolean haveMenu() {
+        return this.menu != null;
     }
 }
