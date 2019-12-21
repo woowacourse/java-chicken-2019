8d2b4a29795f74f39a2bd30ed97a7791e55d1588
dkqyqyt <57524159+dkqyqyt@users.noreply.github.com>
2019-12-21 13:35:03 +0900

    Add 기능을 선택하는 메소드

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,4 +9,13 @@ public class InputView {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
     }
+
+    public static int inputAction(){
+        System.out.println("1 - 주문등록");
+        System.out.println("2 - 결제하기");
+        System.out.println("3 - 프로그램 종료\n");
+
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
 }
