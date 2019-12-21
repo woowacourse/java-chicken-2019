2e5d9afc7f6128f02f87bde1efdf3bb52dfaafb8
zion830 <zion830@honglab.org>
2019-12-21 16:02:45 +0900

    feat: 전체 주문 정보를 가지는 영수증 클래스 추가

---
+++ src/main/java/domain/Receipt.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Receipt {
+    private static final String RECEIPT_TITLE = "주문내역\n메뉴, 수량, 금액";
+    private List<Order> orderList = new ArrayList<>();
+
+    public void addOrder(Menu menu, int conut) {
+        orderList.add(new Order(menu, conut));
+    }
+
+    public int getTotalPrice() {
+        return orderList.stream().mapToInt(Order::getPrice).sum();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder(RECEIPT_TITLE);
+        for (Order order : orderList) {
+            builder.append(order.toString()).append("\n");
+        }
+        return builder.toString();
+    }
+}
