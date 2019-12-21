85621a8d113ed07905f1a826b4aec6961db5e738
zion830 <zion830@honglab.org>
2019-12-21 14:09:40 +0900

    feat: 정수 범위를 표현하는 클래스 추가

---
+++ src/main/java/domain/Range.java
@@ -0,0 +1,28 @@
+package domain;
+
+/*
+ * 범위를 나타내는 클래스
+ * */
+public final class Range {
+    private static final String MSG_RANGE_ERROR = "end는 begin보다 큰 값이어야 합니다.";
+    private final int begin;
+    private final int end;
+
+    public Range(int begin, int end) {
+        if (begin >= end) {
+            throw new IllegalArgumentException(MSG_RANGE_ERROR);
+        }
+
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public boolean verify(int num) {
+        return num <= end && num >= begin;
+    }
+
+    @Override
+    public String toString() {
+        return "Range[" + begin + "," + end + "]";
+    }
+}
