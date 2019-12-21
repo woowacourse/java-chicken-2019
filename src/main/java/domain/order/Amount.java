b276723f100058e7e39b94e5a06e71fca2a0f3e8
MOON JINJU <mjj920930@gmail.com>
2019-12-21 15:27:33 +0900

    feat: 메뉴의 번호와 수량이 주어지면 주문 내역에 추가하는 기능 추가

--- src/main/java/domain/order/Amount.java
+++ src/main/java/domain/order/Amount.java
@@ -1,4 +1,9 @@
 package domain.order;
 
 public class Amount {
+    private int amount;
+
+    public Amount(int amount) {
+        this.amount = amount;
+    }
 }
--- src/main/java/domain/order/Order.java
+++ src/main/java/domain/order/Order.java
@@ -1,13 +1,21 @@
 package domain.order;
 
 import domain.menu.Menu;
+import domain.menu.MenuRepository;
 
 import java.util.HashMap;
 
 public class Order {
-    HashMap<Menu, Amount> order = new HashMap<>();
+    HashMap<Menu, Amount> order;
 
-    public Order(HashMap<Menu, Amount> order) {
-        this.order = order;
+    public Order() {
+        this.order = new HashMap<>();
+    }
+
+    public void add(int menuNumber, int menuAmount) {
+        Menu menu = MenuRepository.findMenuBy(menuNumber);
+        Amount amount = new Amount(menuAmount);
+
+        order.put(menu, amount);
     }
 }
