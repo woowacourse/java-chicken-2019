f0b7bdbe3f3a5d2bd85eb560ec9a11b96b685343
begaonnuri <begaonnuri@gmail.com>
2019-12-21 17:29:03 +0900

    refactor: 다음 동작에 잘못된 값이 입력된 경우 다시 입력받도록 변경
     - POS 관련 동작을 PosService클래스로 이동

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,19 +1,8 @@
 import Service.PosService;
-import domain.NextStep;
-import view.InputView;
-import view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         PosService posService = new PosService();
-        NextStep nextStep;
-
-        do {
-            OutputView.printMain();
-            nextStep = new NextStep(InputView.inputNextStep());
-            if (nextStep.isExit()) return;
-            posService.run(nextStep);
-        }
-        while (!nextStep.isExit());
+        posService.run();
     }
 }
--- src/main/java/Service/PosService.java
+++ src/main/java/Service/PosService.java
@@ -9,25 +9,42 @@ import view.OutputView;
 import java.util.List;
 
 public class PosService {
-    private static final int ORDER = 1;
-    private static final int PAY = 2;
-
-    public void run(NextStep nextStep) {
+    public void run() {
         OrderService orderService = new OrderService();
         PayService payService = new PayService();
-
+        NextStep nextStep;
         final List<Table> tables = TableRepository.tables();
-        OutputView.printTables(tables);
 
-        final int tableNumber = InputView.inputTableNumber();
-        Table selectedTable = selectTable(tables, tableNumber);
+        do {
+            OutputView.printMain();
+            nextStep = choiceNextStep();
+            if (nextStep.isExit()) return;
+            // 주문을 등록하지 않고 결제하는 경우 예외
 
-        if (nextStep.isOrder()) {
-            orderService.order(selectedTable);
+            OutputView.printTables(tables);
+            final int tableNumber = InputView.inputTableNumber();
+            Table selectedTable = selectTable(tables, tableNumber);
+
+            if (nextStep.isOrder()) {
+                orderService.order(selectedTable);
+            }
+            if (nextStep.isPay()) {
+                payService.pay(selectedTable);
+            }
         }
-        if (nextStep.isPay()) {
-            payService.pay(selectedTable);
+        while (!nextStep.isExit());
+    }
+
+    private NextStep choiceNextStep() {
+        NextStep nextStep;
+
+        try {
+            nextStep = new NextStep(InputView.inputNextStep());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            nextStep = new NextStep(InputView.inputNextStep());
         }
+        return nextStep;
     }
 
     private Table selectTable(List<Table> tables, int number) {
