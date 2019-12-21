e807b516d14e1776971ee325af23f56f7d337f01
seonuk <sunwook317@gmail.com>
2019-12-21 13:31:11 +0900

    Write menu Input
    메뉴 인풋 기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,12 +10,14 @@ import java.util.List;
 public class Application {
     // TODO 구현 진행
     public static void main(String[] args) {
-        final List<Table> tables = TableRepository.tables();
-        OutputView.printTables(tables);
 
-        final int tableNumber = InputView.inputTableNumber();
 
-        final List<Menu> menus = MenuRepository.menus();
-        OutputView.printMenus(menus);
+//        final List<Table> tables = TableRepository.tables();
+//        OutputView.printTables(tables);
+//
+//        final int tableNumber = InputView.inputTableNumber();
+//
+//        final List<Menu> menus = MenuRepository.menus();
+//        OutputView.printMenus(menus);
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,4 +9,24 @@ public class InputView {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
     }
+
+    public static int inputMainNumber() {
+        System.out.println("## 주문할 테이블을 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputFunctionNumber() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputMenuNumber() {
+        System.out.println("## 등록할 메뉴를 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputMenuQuantity() {
+        System.out.println("## 메뉴의 수량을 입력하세요.");
+        return scanner.nextInt();
+    }
 }
