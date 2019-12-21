afa6de93a445f5db2111afbd21df36c3e3e73e61
jaeju.jang <jjj0611@gmail.com>
2019-12-21 13:57:17 +0900

    add mainmenu input & output feature with exception

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,20 +2,43 @@ import domain.Menu;
 import domain.MenuRepository;
 import domain.Table;
 import domain.TableRepository;
+import exception.InvalidInputException;
 import view.InputView;
 import view.OutputView;
 
+import java.util.InputMismatchException;
 import java.util.List;
 
 public class Application {
     // TODO 구현 진행
     public static void main(String[] args) {
-        final List<Table> tables = TableRepository.tables();
-        OutputView.printTables(tables);
+        while (true) {
+            int mainMenu = takeMainMenuFromUser();
 
-        final int tableNumber = InputView.inputTableNumber();
+            final List<Table> tables = TableRepository.tables();
+            OutputView.printTables(tables);
 
-        final List<Menu> menus = MenuRepository.menus();
-        OutputView.printMenus(menus);
+            final int tableNumber = InputView.inputTableNumber();
+
+            final List<Menu> menus = MenuRepository.menus();
+            OutputView.printMenus(menus);
+        }
+    }
+
+    private static int takeMainMenuFromUser() {
+        try {
+            OutputView.printMain();
+            int mainMenu = InputView.inputMainNumber();
+            validateMainNumber(mainMenu);
+            return mainMenu;
+        } catch (InputMismatchException ime) {
+            throw new InvalidInputException("숫자가 아닌 값을 입력하실 수 없습니다.");
+        }
+    }
+
+    private static void validateMainNumber(int mainMenu) {
+        if (mainMenu != 1 && mainMenu != 2 && mainMenu != 3) {
+            throw new InvalidInputException("메인 메뉴는 1, 2, 3 중 하나를 선택해주세요");
+        }
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    public static int inputMainNumber() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
+
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
     }
+
 }
