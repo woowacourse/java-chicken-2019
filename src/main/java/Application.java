9e88d11f93e69316602b94cc1213802b04a0e663
KongsonPark <sonypark0204+wootech@gmail.com>
2019-12-21 16:50:30 +0900

    [feat] 결제가 불가능한 경우 메인화면으로 돌아가는 기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import domain.OrderMenu;
@@ -8,6 +9,8 @@ import view.OutputView;
 
 public class Application {
 	// TODO 구현 진행
+	private static boolean flag = true;
+
 	public static void main(String[] args) {
 		Order order = new Order();
 
@@ -15,14 +18,24 @@ public class Application {
 			OutputView.printMainScreen();
 			int process = InputView.inputMainProcess();
 			if (process == 1) {
-				continue;
+				order.orderMenu();
 			}
-			break;
-		} while (order.orderMenu());
 
-		int tableNumberToPay = InputView.inputTableNumberToPay();
-		List<OrderMenu> orderMenuList = order.getTableToPay(tableNumberToPay);
-		Pay.pay(orderMenuList, tableNumberToPay);
+			if (process == 2) {
+				int tableNumberToPay = InputView.inputTableNumberToPay();
+				List<OrderMenu> orderMenuList = order.getTableToPay(tableNumberToPay);
+				if (orderMenuList.isEmpty()) {
+					System.out.println("주문 내역이 없는 테이블 입니다.");
+					continue;
+				}
+				Pay.pay(orderMenuList, tableNumberToPay);
+			}
+
+			if (process == 3) {
+				break;
+			}
+
+		} while (flag);
 
 	}
 }
