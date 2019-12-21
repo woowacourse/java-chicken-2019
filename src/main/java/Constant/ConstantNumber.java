b27c5a4a911009d9b53d366d19897322a022c23e
dkqyqyt <57524159+dkqyqyt@users.noreply.github.com>
2019-12-21 15:18:29 +0900

    Add 상수 클래스 추가

---
+++ src/main/java/Constant/ConstantNumber.java
@@ -0,0 +1,10 @@
+package Constant;
+
+public class ConstantNumber {
+    public static final int MAX_ORDER = 99;
+    public static final int ORDER = 1;
+    public static final int PAY = 2;
+    public static final int EXIT = 3;
+    public static final int CANNOT_ORDER = 0;
+    public static final int ZERO = 0;
+}
