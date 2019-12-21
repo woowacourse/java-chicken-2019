0b4697d3d864bef3ef077a925162385b664064fd
gunhee lee <dlrjsgml42@naver.com>
2019-12-21 18:01:35 +0900

    [Menu] make getPrice method getPrice 메서드를 만들었다.

--- src/main/java/domain/menu/Menu.java
+++ src/main/java/domain/menu/Menu.java
@@ -17,6 +17,15 @@ public class Menu {
         return menuCode == number;
     }
 
+    /**
+     * getPrice는 메뉴의 가격을 반환하는 getter 메서드이다.
+     *
+     * @return 이 메뉴의 가격을 반환한다.
+     */
+    public int getPrice() {
+        return price;
+    }
+
     @Override
     public String toString() {
         return category + " " + number + " - " + name + " : " + price + "원";
