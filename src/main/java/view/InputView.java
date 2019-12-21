e609b4328a721bde8d1fd9032e49aebcd739ff52
barrett24 <barrett2440@gmail.com>
2019-12-21 17:52:41 +0900

    Modified - 출력을 보다 보기좋게 수정, 결제 경우에도 주문으로 뜨는 상황을 해결하기 위한 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,12 +10,12 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputMainOption() {
-        System.out.println("## 원하는 기능을 선택하세요.");
+        System.out.println("\n## 원하는 기능을 선택하세요.");
         return scanner.nextInt();
     }
 
     public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
+        System.out.println("\n## 테이블을 선택하세요.");
         return scanner.nextInt();
     }
 
@@ -28,12 +28,12 @@ public class InputView {
     }
 
     public static int inputAddMenu() {
-        System.out.println("## 등록할 메뉴를 선택해주세요.");
+        System.out.println("\n## 등록할 메뉴를 선택해주세요.");
         return scanner.nextInt();
     }
 
     public static int inputCountMenu() {
-        System.out.println("## 메뉴의 수량을 입력하세요.");
+        System.out.println("\n## 메뉴의 수량을 입력하세요.");
         return scanner.nextInt();
     }
 
