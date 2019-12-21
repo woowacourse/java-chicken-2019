a1165a7e2a6465a5571c1b5de8a636160f5d465e
HWooKim97 <rgd5454@naver.com>
2019-12-21 17:46:33 +0900

    Handle input execption

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,34 +9,46 @@ public class InputView {
     public static final int PAY = 2;
     public static final int CLOSE = 3;
 
+    private static int execptionInput() {
+        while (true) {
+            try {
+                return scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("\n!!잘못 입력하셨습니다. 다시 입력해주세요.");
+                scanner.nextLine();
+                return scanner.nextInt();
+            }
+        }
+    }
+
     public static int inputMainNumber() {
         System.out.println("## 원하는 기능을 선택하세요.");
-        return scanner.nextInt();
+        return execptionInput();
     }
 
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
+        return execptionInput();
     }
 
     public static int inputMenuNumber() {
         System.out.println("## 등록할 메뉴를 선택하세요.");
-        return scanner.nextInt();
+        return execptionInput();
     }
 
     public static int inputMenuAmount() {
         System.out.println("## 메뉴의 수량을 입력하세요.");
-        return scanner.nextInt();
+        return execptionInput();
     }
 
     public static int inputPayTableNumber() {
         System.out.println("## 테이블을 선택하세요.");
-        return scanner.nextInt();
+        return execptionInput();
     }
 
     public static int inputPayNumber(int tableNumber) {
         System.out.println("## " + tableNumber + "번 테이블의 결제를 진행합니다.");
         System.out.println("## 신용 카드는 1번, 현금은 2번");
-        return scanner.nextInt();
+        return execptionInput();
     }
 }
