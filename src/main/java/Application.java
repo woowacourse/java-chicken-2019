70409216696deab5c4e3f5d1286cfe7707e1e60c
junho <hwanghe159@naver.com>
2019-12-21 13:50:24 +0900

    main 함수의 전치적인 구조 구축, 기능에 따라 order(), pay()로 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,15 +8,42 @@ import view.OutputView;
 import java.util.List;
 
 public class Application {
-    // TODO 구현 진행
-    public static void main(String[] args) {
-        final List<Table> tables = TableRepository.tables();
-        OutputView.mainDisplay();
-        OutputView.printTables(tables);
+	// TODO 구현 진행
+	public static void main(String[] args) {
+		final List<Table> tables = TableRepository.tables();
+		OutputView.mainDisplay();
+		int command = InputView.inputTableNumber();
 
-        final int tableNumber = InputView.inputTableNumber();
+		while (true) {
+			if (command == 1) {
+				order();
+				return;
+			}
+			if (command == 2) {
+				pay();
+				return;
+			}
+			if (command == 3) {
+				return;
+			}
+		}
 
-        final List<Menu> menus = MenuRepository.menus();
-        OutputView.printMenus(menus);
-    }
+//		OutputView.printTables(tables);
+//
+//		final int tableNumber = InputView.inputTableNumber();
+//
+//		final List<Menu> menus = MenuRepository.menus();
+//		OutputView.printMenus(menus);
+	}
+
+	private static void pay() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void order() {
+		// TODO Auto-generated method stub
+
+	}
+
 }
