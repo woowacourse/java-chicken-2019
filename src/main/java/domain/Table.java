b7516f0df1fb0e4ff15796086c6a4341f886ee03
KSKIM <kwangsoonkim@outlook.com>
2019-12-21 15:34:08 +0900

    feat: 테이블 번호를 통해 찾고자 하는 테이블인지 확인하는 메서드 추가

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Table {
     private final int number;
 
@@ -7,8 +9,31 @@ public class Table {
         this.number = number;
     }
 
+    public boolean isSameTable(int tableNumber) {
+        return number == tableNumber;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
     @Override
     public String toString() {
         return Integer.toString(number);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Table table = (Table)o;
+        return number == table.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
