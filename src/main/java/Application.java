86316d4563d0f564047f199ffeffd1c0a8a82438
junho <hwanghe159@naver.com>
2019-12-21 13:44:44 +0900

    메인 화면을 출력하는 mainDisplay() 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -11,6 +11,7 @@ public class Application {
     // TODO 구현 진행
     public static void main(String[] args) {
         final List<Table> tables = TableRepository.tables();
+        OutputView.mainDisplay();
         OutputView.printTables(tables);
 
         final int tableNumber = InputView.inputTableNumber();
