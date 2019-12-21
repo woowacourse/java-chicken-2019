5eea91dc094ab09d12a6b9db2d6fc51614b86899
junho <hwanghe159@naver.com>
2019-12-21 16:02:07 +0900

    주문 등록된 테이블에 표시가 될 수 있도록 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,42 +8,55 @@ import domain.TableRepository;
 import java.util.List;
 
 public class OutputView {
-    private static final String TOP_LINE = "┌ ─ ┐";
-    private static final String TABLE_FORMAT = "| %s |";
-    private static final String BOTTOM_LINE = "└ ─ ┘";
-    
-    public static void mainDisplay() {
-    	System.out.println("##메인 화면");
-    	System.out.println("1 - 주문등록");
-    	System.out.println("2 - 결제하기");
-    	System.out.println("3 - 프로그램 종료");
+	private static final String TOP_LINE = "┌ ─ ┐";
+	private static final String TABLE_FORMAT = "| %s |";
+	private static final String BOTTOM_LINE = "└ ─ ┘";
+	private static final String BOTTOM_LINE_WITH_CHECK = "└ @ ┘";
+
+	public static void mainDisplay() {
+		System.out.println("##메인 화면");
+		System.out.println("1 - 주문등록");
+		System.out.println("2 - 결제하기");
+		System.out.println("3 - 프로그램 종료");
 	}
 
-    public static void printTables(final TableRepository tables) {
-        System.out.println("## 테이블 목록");
-        final int size = tables.size();
-        printLine(TOP_LINE, size);
-        printTableNumbers(tables);
-        printLine(BOTTOM_LINE, size);
-    }
+	public static void printTables(final TableRepository tables) {
+		System.out.println("## 테이블 목록");
+		final int size = tables.size();
+		printTopLine(size);
+		printTableNumbers(tables);
+		printBottomLine(tables);
+	}
 
-    public static void printMenus(final MenuRepository menus) {
-        for (final Menu menu : menus.getMenus()) {
-            System.out.println(menu);
-        }
-    }
+	public static void printMenus(final MenuRepository menus) {
+		for (final Menu menu : menus.getMenus()) {
+			System.out.println(menu);
+		}
+	}
 
-    private static void printLine(final String line, final int count) {
-        for (int index = 0; index < count; index++) {
-            System.out.print(line);
-        }
-        System.out.println();
-    }
+	private static void printTopLine(final int count) {
+		for (int index = 0; index < count; index++) {
+			System.out.print(TOP_LINE);
+		}
+		System.out.println();
+	}
 
-    private static void printTableNumbers(final TableRepository tables) {
-        for (final Table table : tables.getTables()) {
-            System.out.printf(TABLE_FORMAT, table);
-        }
-        System.out.println();
-    }
+	private static void printTableNumbers(final TableRepository tables) {
+		for (final Table table : tables.getTables()) {
+			System.out.printf(TABLE_FORMAT, table);
+		}
+		System.out.println();
+	}
+
+	private static void printBottomLine(final TableRepository tables) {
+		for (final Table table : tables.getTables()) {
+			if (table.isEmpty()) {
+				System.out.printf(BOTTOM_LINE, table);
+				continue;
+			}
+			System.out.printf(BOTTOM_LINE_WITH_CHECK, table);
+		}
+		System.out.println();
+	}
+
 }
