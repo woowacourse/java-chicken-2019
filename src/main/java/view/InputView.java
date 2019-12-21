7a6ac6c82f0f27c7fe5cdf137a793aed59994e0c
Kuenhwi <kueniyam@naver.com>
2019-12-21 17:45:36 +0900

    질문이 올바르게 되도록 문장 수정
    "## 주문할 테이블을 선택하세요." -> "## 테이블을 선택하세요."

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
     }
 
     public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
+        System.out.println("## 테이블을 선택하세요.");
         return scanner.nextInt();
     }
 
