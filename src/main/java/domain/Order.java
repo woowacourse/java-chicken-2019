f9bb00ca514d25d617682024b6b8be410ac4d2c9
zion830 <zion830@honglab.org>
2019-12-21 16:02:28 +0900

    feat: 개별 주문 정보를 나타내는 클래스 추가

---
+++ src/main/java/domain/Order.java
@@ -0,0 +1,21 @@
+package domain;
+
+public class Order {
+    private static final String ORDER_FORMAT = "%s - %d개, %d원";
+    private Menu menu;
+    private int count;
+
+    public Order(Menu menu, int count) {
+        this.menu = menu;
+        this.count = count;
+    }
+
+    public int getPrice() {
+        return menu.getPrice() * count;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(ORDER_FORMAT, menu.getName(), count, getPrice());
+    }
+}
