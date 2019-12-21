d89240998ed985087094e2c104992571ab97e6fe
ParkSanggi <roqkfwkehlwk@naver.com>
2019-12-21 16:45:22 +0900

    feat : 29. 입력내용이 유효한 테이블번호인지 확인한다.

--- src/main/java/domain/TableNumberInput.java
+++ src/main/java/domain/TableNumberInput.java
@@ -1,13 +1,25 @@
 package domain;
 
 import view.InputView;
+import view.OutputView;
+
+import java.util.regex.Pattern;
 
 public class TableNumberInput {
+
     public static int make() {
         String inputString;
         do {
             inputString = InputView.inputTableNumber();
         } while (!isValid(inputString));
         return Integer.parseInt(inputString);
+    }
+
+    public static boolean isValid(String inputString) {
+        if (isNumeric(inputString)) {
+            Table inputTable = new Table(Integer.parseInt(inputString));
+            return TableRepository.tables().contains(inputTable);
+        }
+        return false;
     }
 }
