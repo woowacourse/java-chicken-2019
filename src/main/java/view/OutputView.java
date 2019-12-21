89b9babbd72f946b0c00b07143512ddc14f55d41
pandahun <gnsgnsek@gmail.com>
2019-12-21 15:50:22 +0900

    feature: Crreate OuputView.java
    출력 객체 작성
    -프로그램 출력 작성.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,6 +2,7 @@ package view;
 
 import domain.Menu;
 import domain.Table;
+import domain.Tables;
 
 import java.util.List;
 
@@ -9,13 +10,29 @@ public class OutputView {
     private static final String TOP_LINE = "┌ ─ ┐";
     private static final String TABLE_FORMAT = "| %s |";
     private static final String BOTTOM_LINE = "└ ─ ┘";
+    private static final String OPTIONAL_BOTTOM_LINE = "└ \\ ┘";
 
-    public static void printTables(final List<Table> tables) {
+    public static void printMainMenus() {
+        System.out.println("## 메인화면");
+        System.out.println("1 -  주문등록");
+        System.out.println("2 -  결제하기");
+        System.out.println("3 -  프로그램 종료");
+        System.out.println();
+    }
+
+    public static void printTables(final Tables tables) {
         System.out.println("## 테이블 목록");
         final int size = tables.size();
         printLine(TOP_LINE, size);
         printTableNumbers(tables);
-        printLine(BOTTOM_LINE, size);
+        for( Table table : tables.tables()) {
+            if(table.hasOrder()) {
+                System.out.print(OPTIONAL_BOTTOM_LINE);
+                continue;
+            }
+            System.out.print(BOTTOM_LINE);
+        }
+        System.out.println();
     }
 
     public static void printMenus(final List<Menu> menus) {
@@ -31,10 +48,21 @@ public class OutputView {
         System.out.println();
     }
 
-    private static void printTableNumbers(final List<Table> tables) {
-        for (final Table table : tables) {
+    private static void printTableNumbers(final Tables tables) {
+        for (final Table table : tables.tables()) {
             System.out.printf(TABLE_FORMAT, table);
         }
         System.out.println();
+    }
+
+    public static void printOrderAmount( double amount ) {
+        System.out.println("## 최종 결제할 금액");
+        System.out.println(amount +"원");
+    }
+
+    public static void printEndProgram() {
+        System.out.println("=======================");
+        System.out.println("프로그램을 종료합니다.");
+        System.out.println("=======================");
     }
 }
