84734d8b281f1f879965ed21a3d41876c08133b5
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-21 16:30:33 +0900

    feat: Add checkInList for menu selection
    Validation now has checkInList checks whether targe is in the list.

--- src/main/java/work/Validation.java
+++ src/main/java/work/Validation.java
@@ -1,11 +1,19 @@
 package work;
 
+import java.util.List;
+
 public class Validation {
 	private static final String RANGE_FORMAT="%d ~ %d 까지의 수만 가능합니다.";
 
 	public static void checkPositiveRange(int target, int limit){
 		if (target < 0 || target > limit ) {
 			throw new IllegalArgumentException(String.format(RANGE_FORMAT,1, limit));
+		}
+	}
+
+	public static void checkInList(int value, List<Integer> collect) {
+		if(!collect.contains(value)){
+			throw new IllegalArgumentException("유효하지 않은 입력입니다.");
 		}
 	}
 }
