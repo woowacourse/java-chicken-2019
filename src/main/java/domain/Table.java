a2be9bc8d8d9669d2d933f471115ba1f768a217d
김예림 <kiel0103@naver.com>
2019-12-21 15:00:36 +0900

    기능: 테이블 객체에, 해당 테이블에 사용중인지 아닌지에 대한 정보 추가

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -2,9 +2,14 @@ package domain;
 
 public class Table {
     private final int number;
+    private boolean isInUse;
 
     public Table(final int number) {
         this.number = number;
+    }
+
+    public boolean isInUse() {
+        return this.isInUse;
     }
 
     @Override
