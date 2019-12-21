02cccc500b77d5d5144de1906a89f1f133a4bdc2
jiwon_lala <s26788761@naver.com>
2019-12-21 17:03:34 +0900

    refactor : 미사용 함수 삭제 및 함수명 변경
    기존에 작성되어 있던 public static int inputTableNumber() 함수 삭제
    getFunctionNumber 함수 함수명 inputFunctionNumber로 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,12 +6,7 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-//    public static int inputTableNumber() {
-//        System.out.println("## 주문할 테이블을 선택하세요.");
-//        return scanner.nextInt();
-//    }
-
-    public static int getFuctionNumber(int maxValue) { //나중에 inputFunctionNumber()로 수정
+    public static int inputFuctionNumber(int maxValue) {
         int input;
 
         do {
