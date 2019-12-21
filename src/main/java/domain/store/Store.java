440c7d5ae4f6bc945b9ad6c2f81fdf64c964ce98
gunhee lee <dlrjsgml42@naver.com>
2019-12-21 14:31:30 +0900

    [Store] make sales method. and add input method(todoNumber) sales method는, 가게의 영업을 책임지는 메서드이다. 실행되면 무한루프를 돌고, 입력을 통해 주문/결제/종료를 결정한다. 더하여, 이 입력 값을 구하는 메서드 getTodoNumber를 만들었다. 이에 대해 범위 초과나 타입 미스매치와 같은 예외처리를 준비했다.

--- src/main/java/domain/store/Store.java
+++ src/main/java/domain/store/Store.java
@@ -1,5 +1,15 @@
 package domain.store;
 
+import domain.menu.Menu;
+import domain.menu.MenuRepository;
+import domain.table.Table;
+import domain.table.TableRepository;
+import view.InputView;
+import view.OutputView;
+
+import java.util.InputMismatchException;
+import java.util.List;
+
 /**
  * Store 클래스는 매장 전반의 운영을 담당하는 클래스이다.
  * 메뉴와 테이블을 포함하여 이들을 관리/운영하고,
@@ -7,6 +17,10 @@ package domain.store;
  * 입력에 따라 프로그램이 종료되면, 매장의 운영을 종료한다.
  */
 public class Store {
+    private final List<Table> tables;
+
+    private final List<Menu> menus;
+
     /**
      * 정적이고 보호되는 클래스 StoreHolder는,
      * 프로그램 전체에 유일하게 존재함이 보장되어야 하는 Store 인스턴스를 생성한다.
@@ -18,9 +32,11 @@ public class Store {
     /**
      * 생성자 메서드이다.
      * 외부에서 호출되지 못하도록 보호된다.
+     * 테이블 리스트와 메뉴 리스트를 생성해준다.
      */
     private Store() {
-
+        tables = TableRepository.tables();
+        menus = MenuRepository.menus();
     }
 
     /**
@@ -30,5 +46,50 @@ public class Store {
      */
     public static Store getInstance() {
         return StoreHolder.INSTANCE;
+    }
+
+    /**
+     * 주문, 결제, 종료 중 하나를 선택하는 입력값 todoNumber를 받는다.
+     * @return 입력된 todoNumber값을 반환한다.
+     */
+    private int getTodoNumber() {
+        final int todoNumber;
+        try{
+            todoNumber = InputView.inputTodoNumber();
+            checkTodoNumberException(todoNumber);
+        } catch (InputMismatchException e){
+            System.out.println("잘못된 값입니다. 값을 다시 입력해주세요");
+            return getTodoNumber();
+        }
+        return todoNumber;
+    }
+
+    /**
+     * 입력된 todoNumber값에 대하여, 예외처리할 사항을 검사한다.
+     * @param todoNumber 입력된 숫자 todoNumber이다.
+     * @throws InputMismatchException 만약 입력값이 정해진 범위보다 크거나 작으면 예외처리시킨다.
+     */
+    private void checkTodoNumberException(int todoNumber){
+        if(todoNumber < 1 || todoNumber > 3){
+            throw new InputMismatchException("입력은 1,2,3만 가능합니다.");
+        }
+    }
+
+    /**
+     * sales는 매장을 운영하는 메서드이다.
+     * 이 메서드가 호출되면, 매장이 문을 닫을때까지 영업을 진행한다.
+     */
+    public void sales() {
+        int todoNumber;
+        while (true) {
+            todoNumber = getTodoNumber();
+            if(todoNumber == 1){
+                //주문 로직
+            }
+            if(todoNumber == 2){
+                //결제 로직
+            }
+            break;
+        }
     }
 }
