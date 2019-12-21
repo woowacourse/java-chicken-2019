910c2c3970950dcf8b57febb7e7e69c8132bf10c
begaonnuri <begaonnuri@gmail.com>
2019-12-21 13:43:57 +0900

    refactor: PosService클래스에서 관련 동작을 진행하도록 변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,24 +1,14 @@
-import domain.Menu;
-import domain.MenuRepository;
-import domain.Table;
-import domain.TableRepository;
+import Service.PosService;
 import view.InputView;
 import view.OutputView;
 
-import java.util.List;
-
 public class Application {
-    // TODO 구현 진행
     public static void main(String[] args) {
+        PosService posService = new PosService();
+
         OutputView.printMain();
         int nextStep = InputView.inputNextStep();
 
-        final List<Table> tables = TableRepository.tables();
-        OutputView.printTables(tables);
-
-        final int tableNumber = InputView.inputTableNumber();
-
-        final List<Menu> menus = MenuRepository.menus();
-        OutputView.printMenus(menus);
+        posService.run();
     }
 }
---
+++ src/main/java/Service/PosService.java
@@ -0,0 +1,25 @@
+package Service;
+
+import domain.Menu;
+import domain.MenuRepository;
+import domain.Table;
+import domain.TableRepository;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+
+public class PosService {
+    public void run() {
+        final List<Table> tables = TableRepository.tables();
+        OutputView.printTables(tables);
+
+        final int tableNumber = InputView.inputTableNumber();
+
+        final List<Menu> menus = MenuRepository.menus();
+        OutputView.printMenus(menus);
+
+        int menuNumber = InputView.inputMenuNumber();
+        int menuMount = InputView.inputMenuMount();
+    }
+}
