c7d6bbabf9150c6ca15d0d38d5d2643068a6a3e0
HWooKim97 <rgd5454@naver.com>
2019-12-21 14:07:53 +0900

    Add input method

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,24 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    public static int inputMainNumber() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
+
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputMenuNumber() {
+        System.out.println("## 등록할 메뉴를 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputPayNumber(int tableNumber) {
+        System.out.println("## " + tableNumber + "번 테이블의 결제를 진행합니다.");
+        System.out.println("## 신용 카드는 1번, 현금은 2번");
         return scanner.nextInt();
     }
 }
