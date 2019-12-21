161fc5dfed0edbce0ab91d7929da2e0b4c316b82
KSKIM <kwangsoonkim@outlook.com>
2019-12-21 15:38:16 +0900

    refactor: 프로그램의 전반적인 흐름을 ChickinPos가 담당하도록 변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,4 @@
+import controller.ChickinPos;
 import domain.Menu;
 import domain.MenuRepository;
 import domain.Table;
@@ -8,14 +9,11 @@ import view.OutputView;
 import java.util.List;
 
 public class Application {
-    // TODO 구현 진행
-    public static void main(String[] args) {
-        final List<Table> tables = TableRepository.tables();
-        OutputView.printTables(tables);
+	public static void main(String[] args) {
+		final List<Table> tables = TableRepository.tables();
+		final List<Menu> menus = MenuRepository.menus();
 
-        final int tableNumber = InputView.inputTableNumber();
-
-        final List<Menu> menus = MenuRepository.menus();
-        OutputView.printMenus(menus);
-    }
+		ChickinPos chickinPOS = new ChickinPos(tables, menus);
+		chickinPOS.run();
+	}
 }
