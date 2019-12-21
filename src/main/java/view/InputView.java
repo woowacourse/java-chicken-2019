e33a1b0b15eb8160a70ca9d506245813b43e95d7
minuyim <minu_yim@naver.com>
2019-12-21 13:59:57 +0900

    feat : inputMethodNumber 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,6 +4,22 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    
+    public static int inputMethodNumber() {
+        System.out.println("## 메인화면\n 1 - 주문등록\n 2 - 결제하기\n 3 - 프로그램종료");
+        String string = scanner.nextLine();
+        try {
+            Validator.validateMethodNumber(string);
+        } catch (NumberFormatException e) {
+            System.err.println("숫자를 입력해주세요.");
+            inputMethodNumber();
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            inputMethodNumber();
+        }
+        
+        return Integer.valueOf(string);
+    }
 
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
---
+++ src/main/java/view/Validator.java
@@ -0,0 +1,19 @@
+package view;
+
+public class Validator {
+    private static final int MAX_METHOD_NUM = 3;
+    
+    public static void validateMethodNumber(String string) {
+        if (string.isEmpty()) {
+            throw new IllegalArgumentException("null값은 허용되지 않습니다.");
+        }
+        if (Integer.parseInt(string) >  MAX_METHOD_NUM) {
+            throw new IllegalArgumentException("현재 있는 기능만 입력해주세요.");
+        }
+    }
+    
+    public static void validateTableNumber(String string) {
+        
+    }
+
+}
