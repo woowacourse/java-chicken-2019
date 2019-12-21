575a53e19e6cd98e5d0113444792e85811bd1fd3
Unknown <unknown@example.com>
2019-12-21 13:48:02 +0900

    [ADD] 유저 입력을 받는 기능 작성
    이 후 예외처리 할 것

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import org.graalvm.compiler.debug.CSVUtil;
+
 import java.util.Scanner;
 
 public class InputView {
@@ -8,5 +10,19 @@ public class InputView {
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
+    }
+
+    public static int inputMenuNumber() {
+        System.out.println("## 주문할 메뉴를 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputMenuQuantity() {
+        System.out.println("## 메뉴의 갯수를 입력하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputMainFeature() {
+        System.out.println("## 원하는 기능을 선택하세요.");
     }
 }
