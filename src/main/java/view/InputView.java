e47239aa6bb316fce4000c64deaa4d99d1921c1d
hyochan-Kim <kom1230@hanyang.ac.kr>
2019-12-21 17:02:49 +0900

    [ADD] 입력을 담당하는 InputView 클래스에 각 기능별 입력을 받는 메소드를 추가하였습니다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,12 +1,61 @@
 package view;
 
+import utils.Validator;
+
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final Validator validator = new Validator();
+
+    public static int inputFunctionNumber() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        try {
+            return validator.isValidFunctionNumber(scanner.nextInt());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputFunctionNumber();
+        }
+    }
 
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
+        try {
+            return validator.isValidTableNumber(scanner.nextInt());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputTableNumber();
+        }
+    }
+
+    public static int inputMenuNumber() {
+        System.out.println("## 등록할 메뉴를 선택하세요.");
+        try {
+            return validator.isValidMenuNumber(scanner.nextInt());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputMenuNumber();
+        }
+    }
+
+    public static int inputAmount() {
+        System.out.println("\n## 메뉴의 수량을 입력하세요.");
+        try {
+            return validator.isValidAmount(scanner.nextInt());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputAmount();
+        }
+    }
+
+    public static int inputPaymentMethod(int tableNumber) {
+        System.out.println("## " + tableNumber + "번 테이블의 결제를 진행합니다.");
+        System.out.println("## 신용 카드는 1번, 현금은 2번");
+        try {
+            return validator.isValidPaymentMethod(scanner.nextInt());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputPaymentMethod(tableNumber);
+        }
     }
 }
