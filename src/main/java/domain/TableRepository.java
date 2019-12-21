24a0c669ff4752df302bbb9bfb088eb64f13aeca
junho <hwanghe159@naver.com>
2019-12-21 15:10:17 +0900

    TableRepository클래스 내부에 테이블 번호로 테이블을 찾을 수있는 getByNumber() 추가, 예외처리도 적용

--- src/main/java/domain/TableRepository.java
+++ src/main/java/domain/TableRepository.java
@@ -5,18 +5,27 @@ import java.util.Collections;
 import java.util.List;
 
 public class TableRepository {
-    private static final List<Table> tables = new ArrayList<>();
+	private static final List<Table> tables = new ArrayList<>();
 
-    static {
-        tables.add(new Table(1));
-        tables.add(new Table(2));
-        tables.add(new Table(3));
-        tables.add(new Table(5));
-        tables.add(new Table(6));
-        tables.add(new Table(8));
-    }
+	static {
+		tables.add(new Table(1));
+		tables.add(new Table(2));
+		tables.add(new Table(3));
+		tables.add(new Table(5));
+		tables.add(new Table(6));
+		tables.add(new Table(8));
+	}
 
-    public static List<Table> tables() {
-        return Collections.unmodifiableList(tables);
-    }
+	public static List<Table> tables() {
+		return Collections.unmodifiableList(tables);
+	}
+
+	public static Table getByNumber(int number) {
+		for (Table table : tables) {
+			if (table.getNumber() == number) {
+				return table;
+			}
+		}
+		throw new IllegalArgumentException("해당하는 테이블을 찾을 수 없습니다.");
+	}
 }
