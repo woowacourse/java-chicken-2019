d4c30ab195ee22ffddbfa9a52d08057880501e9e
Min-92 <0201.mj.lee@gmail.com>
2019-12-21 16:17:03 +0900

    fix : 오타수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,48 +6,49 @@ import domain.Table;
 import java.util.List;
 
 public class OutputView {
-    private static final String TOP_LINE = "┌ ─ ┐";
-    private static final String TABLE_FORMAT = "| %s |";
-    private static final String BOTTOM_LINE = "└ ─ ┘";
-    private static final String ORDERED_BOTTOM_LINE = "└ $ ┘";
 
-    public static void printTables(final List<Table> tables) {
-        System.out.println("## 테이블 목록");
-        final int size = tables.size();
-        printLine(TOP_LINE, size);
-        printTableNumbers(tables);
-        printButtomLine(tables);
+  private static final String TOP_LINE = "┌ ─ ┐";
+  private static final String TABLE_FORMAT = "| %s |";
+  private static final String BOTTOM_LINE = "└ ─ ┘";
+  private static final String ORDERED_BOTTOM_LINE = "└ $ ┘";
+
+  public static void printTables(final List<Table> tables) {
+    System.out.println("## 테이블 목록");
+    final int size = tables.size();
+    printLine(TOP_LINE, size);
+    printTableNumbers(tables);
+    printBottomLine(tables);
+  }
+
+  public static void printBottomLine(List<Table> tables) {
+    for (Table table : tables) {
+      if (table.hasOrdered()) {
+        System.out.print(ORDERED_BOTTOM_LINE);
+        continue;
+      }
+      System.out.print(BOTTOM_LINE);
     }
+    System.out.println();
+  }
 
-    public static void  printButtomLine(List<Table> tables){
-        for(int i = 0; i < tables.size(); i++){
-            if(tables.get(i).hasOrdered()){
-                System.out.print(ORDERED_BOTTOM_LINE);
-                continue;
-            }
-            System.out.print(BOTTOM_LINE);
-        }
-        System.out.println();
+
+  public static void printMenus(final List<Menu> menus) {
+    for (final Menu menu : menus) {
+      System.out.println(menu);
     }
+  }
 
-
-    public static void printMenus(final List<Menu> menus) {
-        for (final Menu menu : menus) {
-            System.out.println(menu);
-        }
+  private static void printLine(final String line, final int count) {
+    for (int index = 0; index < count; index++) {
+      System.out.print(line);
     }
+    System.out.println();
+  }
 
-    private static void printLine(final String line, final int count) {
-        for (int index = 0; index < count; index++) {
-            System.out.print(line);
-        }
-        System.out.println();
+  private static void printTableNumbers(final List<Table> tables) {
+    for (final Table table : tables) {
+      System.out.printf(TABLE_FORMAT, table);
     }
-
-    private static void printTableNumbers(final List<Table> tables) {
-        for (final Table table : tables) {
-            System.out.printf(TABLE_FORMAT, table);
-        }
-        System.out.println();
-    }
+    System.out.println();
+  }
 }
