bc10ab05558b3785f53fcdf928fedf1cc19c2d22
LTTTTTE <10100aaaaa@gmail.com>
2019-12-21 14:32:12 +0900

    Menus 메뉴 번호로 Menu 찾아주는 기능 추가

--- src/main/java/domain/Menus.java
+++ src/main/java/domain/Menus.java
@@ -13,6 +13,13 @@ public class Menus {
         return menus;
     }
 
+    public Menu findMenu(int menuId) {
+        return menus.stream()
+                .filter(x -> x.isMenuNumberSame(menuId))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
     public boolean isMenuExist(int number) {
         return menus.stream().anyMatch(x -> x.isMenuNumberSame(number));
     }
