6f883daa14e62d50b8f5401bd2d315181e8be813
jaeju.jang <jjj0611@gmail.com>
2019-12-21 17:19:50 +0900

    add exception statement as static variable on InvalidInputException.class
    	- not a number exception statement
    	- wrong main menu number exception statement
    	- wrong menu number exception statement
    	- wrong table number exception statement

--- src/main/java/exception/InvalidInputException.java
+++ src/main/java/exception/InvalidInputException.java
@@ -2,6 +2,11 @@ package exception;
 
 public class InvalidInputException extends IllegalArgumentException {
 
+    public static final String NOT_NUMBER_EXCEPTION = "숫자가 아닌 값을 입력하실 수 없습니다.";
+    public static final String WRONG_MAIN_MENU_NUMBER_EXCEPTION = "메인 메뉴 번호를 잘못 선택하셨습니다.";
+    public static final String WRONG_MENU_NUMBER_EXCEPTION = "존재하지 않는 메뉴 번호를 입력하셨습니다.";
+    public static final String WRONG_TABLE_NUMBER_EXCEPTION = "존재하지 않는 테이블 번호를 입력하셨습니다.";
+
     public InvalidInputException() {
         super();
     }
