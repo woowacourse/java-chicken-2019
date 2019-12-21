8701f62d5ffe21a6eb87bcf6c1d237f054301597
hyochan-Kim <kom1230@hanyang.ac.kr>
2019-12-21 17:19:47 +0900

    [ADD] Table 클래스 생성자 변경 및 상태 확인하는 메소드 추가

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,14 +1,32 @@
 package domain;
 
 public class Table {
+    private static final int TABLE_MIN = 1;
+    private static final int INITIAL_VALUE = 0;
+
     private final int number;
+    private Payment paymentAmount;
+    private String orderHistory;
 
     public Table(final int number) {
+        if (number < TABLE_MIN) {
+            throw new IllegalArgumentException("테이블 숫자는 " + TABLE_MIN + "이상이어야 합니다.");
+        }
         this.number = number;
+        this.paymentAmount = new Payment(INITIAL_VALUE, INITIAL_VALUE);
+        this.orderHistory = "";
     }
 
     @Override
     public String toString() {
         return Integer.toString(number);
+    }
+
+    public boolean isSameNumber(final int number) {
+        return this.number == number;
+    }
+
+    public boolean isBeforePayment() {
+        return orderHistory.equals("");
     }
 }
