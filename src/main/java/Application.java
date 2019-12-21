12ea47fc4b057431299d229f0c60e4bde3d1aa20
junho <hwanghe159@naver.com>
2019-12-21 14:09:10 +0900

    전체적인 진행을 관리하는 ChickenHouse클래스 생성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,41 +8,33 @@ import view.OutputView;
 import java.util.List;
 
 public class Application {
-	// TODO 구현 진행
 	public static void main(String[] args) {
 		final List<Table> tables = TableRepository.tables();
-		OutputView.mainDisplay();
-		int command = InputView.inputTableNumber();
-
 		while (true) {
+			OutputView.mainDisplay();
+			int command = InputView.inputTableNumber();
 			if (command == 1) {
 				order();
-				return;
 			}
 			if (command == 2) {
 				pay();
-				return;
 			}
 			if (command == 3) {
 				return;
 			}
 		}
-
-//		OutputView.printTables(tables);
-//
-//		final int tableNumber = InputView.inputTableNumber();
-//
-//		final List<Menu> menus = MenuRepository.menus();
-//		OutputView.printMenus(menus);
-	}
-
-	private static void pay() {
-		// TODO Auto-generated method stub
-
 	}
 
 	private static void order() {
-		// TODO Auto-generated method stub
+		OutputView.printTables(tables);
+
+		final int tableNumber = InputView.inputTableNumber();
+
+		final List<Menu> menus = MenuRepository.menus();
+		OutputView.printMenus(menus);
+	}
+
+	private static void pay() {
 
 	}
 
