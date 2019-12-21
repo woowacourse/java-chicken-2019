8809e89b5afbdba1633f1c0f5ca0901a4b0aaecf
ParkSanggi <roqkfwkehlwk@naver.com>
2019-12-21 17:29:43 +0900

    feat : 10. 입력받은 내용이 테이블 목록에 있는 숫자인지 확인한다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static int inputTableNumber() {
+    public static String inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
+        return scanner.nextLine();
+    }
+
+    public static String inputMainFeatureNumber(){
+        System.out.println("## 사용할 기능 번호를 선택하세요.");
+        return scanner.nextLine();
     }
 }
