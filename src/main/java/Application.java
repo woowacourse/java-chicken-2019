7b0e7d2a05f843264668bac8912aea07ae4ad433
kimjunseok <jnunseok@gmail.com>
2019-12-21 14:09:09 +0900

    [Add] 기능별 함수 나누기

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,14 +8,40 @@ import view.OutputView;
 import java.util.List;
 
 public class Application {
-    // TODO 구현 진행
     public static void main(String[] args) {
+        int functionNumber = InputView.inputFunctionNumber();;
+        if (functionNumber != 3) {
+            spiltFunction(functionNumber);
+        }
+        System.out.println("프로그램 종료");
+    }
+
+    private static void spiltFunction(int functionNumber) {
         final List<Table> tables = TableRepository.tables();
+        for (Table table: tables) {
+            System.out.println(table.toString());
+        }
+
         OutputView.printTables(tables);
+        if (functionNumber == 1) {
+            doIfNumberIsOne();
+        }
+        if (functionNumber == 2) {
+            final int tableBillNumber = InputView.inputTableBillNumber();
+        }
 
+    }
+
+    private static void doIfNumberIsOne() {
         final int tableNumber = InputView.inputTableNumber();
-
         final List<Menu> menus = MenuRepository.menus();
         OutputView.printMenus(menus);
+        final int menuNumber = InputView.inputMenuNumber();
+        final int MenuCount =  InputView.inputMenuCount();
+        setMenu(tableNumber, menuNumber, MenuCount);
+    }
+
+    private static void setMenu(int tableNumber, int menuNumber, int MenuCount) {
+
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,37 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    public static void inputMainPrint() {
+        System.out.println("## 메인화면");
+        System.out.println("1 - 주문등록");
+        System.out.println("2 - 결제하기");
+        System.out.println("3 - 프로그램 종료");
+        System.out.println();
+    }
+
+    public static int inputFunctionNumber() {
+        inputMainPrint();
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
+
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputTableBillNumber() {
+        System.out.println("## 계산할 테이블을 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputMenuNumber() {
+        System.out.println("## 등록할 메뉴를 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputMenuCount() {
+        System.out.println("## 메뉴의 수량을 입력하세요.");
         return scanner.nextInt();
     }
 }
