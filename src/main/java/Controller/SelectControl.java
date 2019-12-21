c8586464d2ab6e5c5f946e04568aaefd63d15042
YerinCho <forjustice9839@gmail.com>
2019-12-21 14:02:17 +0900

    feat : set up SelectControl class

---
+++ src/main/java/Controller/SelectControl.java
@@ -0,0 +1,12 @@
+package Controller;
+
+import domain.Menu;
+import domain.MenuRepository;
+import domain.Table;
+import domain.TableRepository;
+import view.InputView;
+import view.OutputView;
+
+public class SelectControl {
+   
+}
