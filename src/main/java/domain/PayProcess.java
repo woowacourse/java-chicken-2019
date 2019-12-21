d22cad129c20b5a5a1989eee48c32f0bd451521a
Min-92 <0201.mj.lee@gmail.com>
2019-12-21 16:09:14 +0900

    feat : 결제 선택시 주문여부 예외처리

--- src/main/java/domain/PayProcess.java
+++ src/main/java/domain/PayProcess.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.awt.image.ShortLookupTable;
 import java.util.HashMap;
 import java.util.List;
 
@@ -17,11 +18,21 @@ public class PayProcess {
     this.menus = menus;
     this.tableNumbers = tableNumbers;
     this.menuNumbers = menuNumbers;
-
-
   }
 
   public void start() {
+    if(!hasOrdered()){
+      System.out.println("주문이 존재하지 않습니다.");
+      return;
+    }
+  }
 
+  private boolean hasOrdered(){
+    for(int i = 0; i < tables.size(); i++){
+      if(tables.get(i).hasOrdered()){
+        return true;
+      }
+    }
+    return false;
   }
 }
