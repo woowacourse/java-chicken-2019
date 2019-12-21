f0fc8ce5b402b4e8aa8735f8f7bd0f46dcf7a0ef
김예림 <kiel0103@naver.com>
2019-12-21 14:56:11 +0900

    기능: 입력한 기능이 특정 기능이 맞는지 아닌지 알려주는 메서드 작성

--- src/main/java/domain/ProvidingFunctionRepository.java
+++ src/main/java/domain/ProvidingFunctionRepository.java
@@ -5,6 +5,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class ProvidingFunctionRepository {
+    private static final int ORDER_REGISTRATION_FUNCTION = 1;
+    private static final int PAYMENT_FUNCTION = 2;
+    private static final int EXIT = 3;
+
     private static final List<ProvidingFunction> providingFunctions
             = new ArrayList<ProvidingFunction>();
 
@@ -16,5 +20,32 @@ public class ProvidingFunctionRepository {
 
     public static List<ProvidingFunction> providingFunctions() {
         return Collections.unmodifiableList(providingFunctions);
+    }
+
+    public static boolean isOrderRegistration(int requestNumber) {
+
+        if (requestNumber == ORDER_REGISTRATION_FUNCTION) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isPayment(int requestNumber) {
+
+        if (requestNumber == PAYMENT_FUNCTION) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isExit(int requestNumber) {
+
+        if (requestNumber == EXIT) {
+            return true;
+        }
+
+        return false;
     }
 }
