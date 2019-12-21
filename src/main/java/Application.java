8ece611e03a8971bdb0eb74576df633824bc3b8d
kouz <lastpuzly@gmail.com>
2019-12-21 14:08:12 +0900

    feat: Pos Class 생성, Application 기능 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,32 +1,7 @@
-import domain.Menu;
-import domain.MenuAmount;
-import domain.MenuRepository;
-import domain.Table;
-import domain.TableRepository;
-import view.InputView;
-import view.OutputView;
-
-import java.util.List;
-
 public class Application {
-	private static final int ORDER_REGISTER_NUMBER = 1;
-
     // TODO 구현 진행
     public static void main(String[] args) {
-    	final int functionNumber = Pos.getFunctionNumber();
-		if (functionNumber == ORDER_REGISTER_NUMBER) {
-			registerOrder();
-		}
+    	Pos pos = new Pos();
+    	pos.play();
     }
-
-    private static void registerOrder() {
-		final List<Table> tables = TableRepository.tables();
-		OutputView.printTables(tables);
-		final int tableNumber = InputView.inputTableNumber();
-
-		final List<Menu> menus = MenuRepository.menus();
-		OutputView.printMenus(menus);
-		MenuAmount menuAmount = new MenuAmount(InputView.inputMenuAmount());
-
-	}
 }
--- src/main/java/Pos.java
+++ src/main/java/Pos.java
@@ -1,9 +1,41 @@
+import java.util.List;
+
+import domain.Menu;
+import domain.MenuAmount;
+import domain.MenuRepository;
+import domain.Table;
+import domain.TableRepository;
 import view.InputView;
 import view.OutputView;
 
 public class Pos {
-	public static int getFunctionNumber() {
+	private static final int ORDER_REGISTER_NUMBER = 1;
+
+	private final int functionNumber;
+
+	public Pos() {
+		this.functionNumber = getFunctionNumber();
+	}
+
+	private static int getFunctionNumber() {
 		OutputView.printMainScreen();
 		return InputView.inputFunctionNumber();
+	}
+
+	public void play() {
+		if (functionNumber == ORDER_REGISTER_NUMBER) {
+			registerOrder();
+		}
+	}
+
+	private void registerOrder() {
+		final List<Table> tables = TableRepository.tables();
+		OutputView.printTables(tables);
+		final int tableNumber = InputView.inputTableNumber();
+
+		final List<Menu> menus = MenuRepository.menus();
+		OutputView.printMenus(menus);
+		MenuAmount menuAmount = new MenuAmount(InputView.inputMenuAmount());
+
 	}
 }
