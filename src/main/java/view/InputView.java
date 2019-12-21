8b41743d468fa9b3ad3a90d5fc8986aa495f52ee
hyochan-Kim <kom1230@hanyang.ac.kr>
2019-12-21 17:03:15 +0900

    [REFACTOR] 정수가 아닌 입력이 들어올 경우를 처리 하였습니다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,6 +11,7 @@ public class InputView {
     public static int inputFunctionNumber() {
         System.out.println("## 원하는 기능을 선택하세요.");
         try {
+            inputOnlyInteger();
             return validator.isValidFunctionNumber(scanner.nextInt());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -21,6 +22,7 @@ public class InputView {
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
         try {
+            inputOnlyInteger();
             return validator.isValidTableNumber(scanner.nextInt());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -31,6 +33,7 @@ public class InputView {
     public static int inputMenuNumber() {
         System.out.println("## 등록할 메뉴를 선택하세요.");
         try {
+            inputOnlyInteger();
             return validator.isValidMenuNumber(scanner.nextInt());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -41,6 +44,7 @@ public class InputView {
     public static int inputAmount() {
         System.out.println("\n## 메뉴의 수량을 입력하세요.");
         try {
+            inputOnlyInteger();
             return validator.isValidAmount(scanner.nextInt());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -52,10 +56,18 @@ public class InputView {
         System.out.println("## " + tableNumber + "번 테이블의 결제를 진행합니다.");
         System.out.println("## 신용 카드는 1번, 현금은 2번");
         try {
+            inputOnlyInteger();
             return validator.isValidPaymentMethod(scanner.nextInt());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return inputPaymentMethod(tableNumber);
+        }
+    }
+
+    private static void inputOnlyInteger() {
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.out.println("정수를 입력하세요.");
         }
     }
 }
