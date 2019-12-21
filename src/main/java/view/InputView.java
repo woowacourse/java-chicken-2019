ac924d43fad07779fed653fcdaef5d2ee5092163
taeheon <dqrd123@gmail.com>
2019-12-21 13:55:37 -0800

    기능목록과 프로그램 입력 구현
    기능목록 README.md를 작성하였고, 주문등록의 테이블 번호입력, 메뉴 입력, 메뉴 수량 입력,
    결제하기의 테이블 번호 입력, 결제 방식 입력을 구현하였다. 모든 입력은 정수로 입력 받는다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,36 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    public static void mainDisplay(){
+        System.out.println("\n##메인화면");
+        System.out.println("1 - 주문등록");
+        System.out.println("2 - 결제하기");
+        System.out.println("3 - 프로그램 종료");
+    }
+
+    public static int inputFunctionNumber(){
+        System.out.println("\n## 원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
+
     public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
+        System.out.println("\n## 주문할 테이블을 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputMenuNumber(){
+        System.out.println("\n## 등록할 메뉴를 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputNumberOfMenu(){
+        System.out.println("\n## 메뉴의 수량을 입력하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputTypeOfPayment(){
+        System.out.println("\n## 1번테이블의 결제를 진행합니다.");
+        System.out.println("## 신용 카드는 1번, 현금은 2번");
         return scanner.nextInt();
     }
 }
