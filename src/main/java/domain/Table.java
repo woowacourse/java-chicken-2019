3feeba1672759d962fddde4ba5029710c354e629
junk-sound <wom2277@naver.com>
2019-12-21 13:39:43 +0900

    기능: 테이블의 상태(대기, 등록 등)를 변화시킨다.

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -2,13 +2,25 @@ package domain;
 
 public class Table {
     private final int number;
+    private TableStatus tableStatus;
 
     public Table(final int number) {
         this.number = number;
+        tableStatus = TableStatus.Wating;
     }
 
     @Override
     public String toString() {
         return Integer.toString(number);
+    }
+
+    //todo: do other logic
+    public void takeAnOrder() {
+        tableStatus = TableStatus.Registered;
+    }
+
+    //todo: do other logic
+    public void getSettled() {
+        tableStatus =  TableStatus.Wating;
     }
 }
---
+++ src/main/java/domain/TableStatus.java
@@ -0,0 +1,6 @@
+package domain;
+
+public enum TableStatus {
+    Wating,
+    Registered
+}
