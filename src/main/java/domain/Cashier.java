6f2af6b6c6692d887b539d0e18cc52f99c61079c
woonjang <woonjangahn@gmail.com>
2019-12-21 16:17:28 +0900

    기능: 지불 단계에서 사용되는 Cashier 객체 생성 및 Money 객체 포장

--- src/main/java/domain/Cashier.java
+++ src/main/java/domain/Cashier.java
@@ -1,5 +1,31 @@
 package domain;
 
-public class Cashier {
+public class Cashier implements CashierService {
+    private static final int TEN = 10;
+    private static final int TEN_THOUSAND = 10000;
+    private static final double CASH_DISCOUNT_RATIO = 0.05;
+    private Order order;
 
+    public Cashier(Order order) {
+        this.order = order;
+    }
+
+    private Money getTotalOriginal() {
+        return new Money(order.getTotalCost());
+    }
+
+    public int getChickenQuantity() {
+        return order.getChickenQuantity();
+    }
+
+    @Override
+    public Money discountByCash() {
+        return discountByChickenQuantity().discountByRatio(CASH_DISCOUNT_RATIO);
+    }
+
+    @Override
+    public Money discountByChickenQuantity() {
+        int amountToDiscount = (getChickenQuantity() % TEN) * TEN_THOUSAND;
+        return getTotalOriginal().discountByAmount(amountToDiscount);
+    }
 }
--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -1,4 +1,23 @@
 package domain;
 
 public class Money {
+    private int amount;
+
+    public Money(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public Money discountByAmount(int amount) {
+        this.amount -= amount;
+        return new Money(this.amount);
+    }
+
+    public Money discountByRatio(double ratio) {
+        this.amount *= (1 - ratio);
+        return new Money(this.amount);
+    }
 }
