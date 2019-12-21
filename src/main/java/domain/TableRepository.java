a1e9cf4fa284ef2358df23372616d552301da9c9
HWooKim97 <rgd5454@naver.com>
2019-12-21 16:38:56 +0900

    Add method getTotalMoney()

--- src/main/java/domain/TableRepository.java
+++ src/main/java/domain/TableRepository.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 public class TableRepository {
     private static final List<Table> tables = new ArrayList<>();
 
@@ -28,5 +29,15 @@ public class TableRepository {
 
     public HashMap<Menu, Integer> getOrderMenuList(int tableNumber) {
         return tables.get(tableNumber).getOrderMenu();
+    }
+
+    public static int getTotalMoney(int tableNumber) {
+        HashMap<Menu, Integer> orderMenu = tables.get(tableNumber).getOrderMenu();
+        int sum = 0;
+        
+        for (Map.Entry<Menu, Integer> order : orderMenu.entrySet()) {
+            sum += order.getKey().getPrice() * order.getValue();
+        }
+        return sum;
     }
 }
