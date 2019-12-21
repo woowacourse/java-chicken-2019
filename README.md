57c7b73b73bf226f23f8d17358182abd41ab5ca9
Yeji Ahn <yeppilisa@naver.com>
2019-12-21 13:26:26 +0900

    Add OutputView printMain

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # java-chicken-2019
+
+## 구현할 기능 목록
+- 메인 화면 OutputView
+- 메인 화면 InputView
+- 
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,6 +10,14 @@ public class OutputView {
     private static final String TABLE_FORMAT = "| %s |";
     private static final String BOTTOM_LINE = "└ ─ ┘";
 
+    public static void printMain() {
+        System.out.println("## 메인화면");
+        System.out.println("1 - 주문등록");
+        System.out.println("2 - 결제하기");
+        System.out.println("3 - 프로그램 종료");
+        System.out.println();
+    }
+
     public static void printTables(final List<Table> tables) {
         System.out.println("## 테이블 목록");
         final int size = tables.size();
