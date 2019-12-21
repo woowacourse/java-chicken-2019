ad6fc893e42df8cca7d2c1ac98ead48ec1d17664
jaeju.jang <jjj0611@gmail.com>
2019-12-21 17:15:52 +0900

    change instance variable on TableMenu.class to Count object

--- src/main/java/domain/TableMenu.java
+++ src/main/java/domain/TableMenu.java
@@ -3,19 +3,23 @@ package domain;
 public class TableMenu {
 
     private Menu menu;
-    private int count;
+    private Count count;
 
     public TableMenu(Menu menu, int count) {
         this.menu = menu;
-        this.count = count;
+        this.count = new Count(count);
+    }
+
+    public void addCount(int count) {
+        this.count.addCount(count);
     }
 
     public int calculateMoney() {
-        return menu.getPrice() * count;
+        return menu.getPrice() * count.getCount();
     }
 
     @Override
     public String toString() {
-        return menu.getName() + " " + count + " " + menu.getPrice();
+        return menu.getName() + " " + count.getCount() + " " + menu.getPrice();
     }
 }
