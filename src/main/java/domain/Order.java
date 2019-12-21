8558e0a944418d85d93808d629032d7886ccfdbf
pandahun <gnsgnsek@gmail.com>
2019-12-21 16:54:56 +0900

    feature: Modify Order
    Order 객체 수정
    -주문이 치킨 메뉴인지 확인

--- src/main/java/domain/Order.java
+++ src/main/java/domain/Order.java
@@ -2,12 +2,19 @@ package domain;
 
 public class Order {
 
+    private static final int MINIMUM_CHICKEN = 1;
+    private static final int MAXIMUM_CHICKEN = 6;
+
     final int number;
     final int quantity;
 
     public Order( int number, int quantity ) {
         this.number = number;
         this.quantity = quantity;
+    }
+
+    public boolean isChicken() {
+        return (MINIMUM_CHICKEN <= number) && (MAXIMUM_CHICKEN >= number);
     }
 
     @Override
