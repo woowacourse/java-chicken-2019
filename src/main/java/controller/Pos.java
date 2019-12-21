04892687b7cd1407892b6588ce41c8704c6fd200
Min-92 <0201.mj.lee@gmail.com>
2019-12-21 13:48:41 +0900

    feat : 프로세스 선택하는 기능 추가
    - 프로세스 목록 출력
    - 예외처리
    	숫자가 아닌 경우
    	1,2,3 이 아닌 경우

--- src/main/java/controller/Pos.java
+++ src/main/java/controller/Pos.java
@@ -10,7 +10,14 @@ import view.OutputView;
 
 public class Pos {
 
+  private final int ORDER = 1;
+  private final int PAY = 2;
+  private final int END = 3;
+
+
   public void start() {
+    int process = getProcess();
+
     final List<Table> tables = TableRepository.tables();
     OutputView.printTables(tables);
 
@@ -19,4 +26,25 @@ public class Pos {
     final List<Menu> menus = MenuRepository.menus();
     OutputView.printMenus(menus);
   }
+
+  private int getProcess(){
+    return validateProcessNumber(InputView.inputProcessNumber());
+  }
+
+  private int isInProcess(int processNumber) throws Exception {
+    if(processNumber != ORDER && processNumber != PAY && processNumber != END){
+      throw new Exception("1, 2, 3 중 하나만 입력해주세요.");
+    }
+    return processNumber;
+  }
+
+  private int validateProcessNumber(int processNumber){
+    try{
+      return isInProcess(processNumber);
+    }catch (Exception e){
+      System.out.println(e.getMessage());
+      return getProcess();
+    }
+  }
+
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,10 +3,27 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
 
-    public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static int inputTableNumber() {
+    System.out.println("## 주문할 테이블을 선택하세요.");
+    return inputNumber();
+  }
+
+  private static int inputNumber() {
+    try {
+      return Integer.parseInt(scanner.nextLine());
+    } catch (
+        Exception e) {
+      System.out.println("숫자만 입력하세요");
+      return inputNumber();
     }
+  }
+
+  public static int inputProcessNumber(){
+    System.out.println("## 메인화면\n1 - 주문등록\n2 - 결제하기\n3 - 프로그램 종료");
+    return inputNumber();
+  }
+
 }
