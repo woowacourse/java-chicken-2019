5915989c50c3d8fabdd723ed39cd022e5026efce
HWooKim97 <rgd5454@naver.com>
2019-12-21 14:58:12 +0900

    Add field
    - Save ordered menu, money

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,7 +1,12 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Table {
     private final int number;
+    private List<Menu> orderMenu = new ArrayList<>();
+    private int money = 0;
 
     public Table(final int number) {
         this.number = number;
