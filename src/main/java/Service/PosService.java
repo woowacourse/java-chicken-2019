662c1933c0aebbe96ef4a81be66af1725b1538b5
begaonnuri <begaonnuri@gmail.com>
2019-12-21 14:15:42 +0900

    feat: 테이블에 주문하는 기능 구현  - 메뉴를 선택하는 기능 구현  - Table객체에 주문한 메뉴를 HashMap<메뉴,수량> 형태로 저장

--- src/main/java/Service/PosService.java
+++ src/main/java/Service/PosService.java
@@ -21,12 +21,23 @@ public class PosService {
         OutputView.printMenus(menus);
 
         int menuNumber = InputView.inputMenuNumber();
+        Menu selectedMenu = selectMenu(menus, menuNumber);
+        System.out.println("here : " + selectedMenu.toString());
+
         int menuMount = InputView.inputMenuMount();
+        selectedTable.addMenu(selectedMenu, menuMount);
     }
 
     private Table selectTable(List<Table> tables, int number) {
         return tables.stream()
                 .filter(table -> table.isSameTableNumber(number))
+                .findFirst()
+                .get();
+    }
+
+    private Menu selectMenu(List<Menu> menus, int number) {
+        return menus.stream()
+                .filter(menu -> menu.isSameMenuNumber(number))
                 .findFirst()
                 .get();
     }
--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,7 +1,10 @@
 package domain;
 
+import java.util.HashMap;
+
 public class Table {
     private final int number;
+    private final HashMap<Menu, Integer> bill = new HashMap<>();
 
     public Table(final int number) {
         this.number = number;
@@ -9,6 +12,22 @@ public class Table {
 
     public boolean isSameTableNumber(int number) {
         return this.number == number;
+    }
+
+    public void addMenu(Menu menu, int mount) {
+        if (hasMenu(menu)) {
+            bill.replace(menu, mount);
+            return;
+        }
+        bill.put(menu, mount);
+    }
+
+    private boolean hasMenu(Menu menu) {
+        return bill.containsKey(menu);
+    }
+
+    public String getCurrentOrder() {
+        return bill.toString();
     }
 
     @Override
