fa2a4be64d297496b5f746a2b880cf450c6a3ea0
jiwon_lala <s26788761@naver.com>
2019-12-21 17:50:42 +0900

    feat : Table 클래스 메뉴 추가 함수 및 추가가능 체크 함수 구현
    public void order(Menu menu, int menuCount);
    //테이블에 주문된 menuCount개의 menu를 추가하는 함수.
    
    public boolean canOrder(Menu menu, int menuCount);
    //테이블에 menu를 menuCount 만큼 추가할 수 있는지(수량이 99개가 넘었는지) 체크하여 가능하면 true, 불가능하면 false 리턴하는 함수

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,10 +1,17 @@
 package domain;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Table {
     private final int number;
+    private Map<Menu, Integer> orderedList;
+
 
     public Table(final int number) {
         this.number = number;
+        orderedList = new HashMap<>();
     }
 
     @Override
@@ -15,4 +22,30 @@ public class Table {
     public int getNumber() {
         return number;
     }
+
+    public void order(Menu menu, int menuCount) {
+        if (orderedList.containsKey(menu)) {
+            int remainCount = orderedList.get(menu);
+            orderedList.remove(menu, menuCount);
+            orderedList.put(menu, remainCount + menuCount);
+            return;
+        }
+        orderedList.put(menu, menuCount);
+    }
+
+    public boolean canOrder(Menu menu, int menuCount) {
+        int totalCount = 0;
+
+        if (orderedList.containsKey(menu)) {
+            totalCount += orderedList.get(menu);
+        }
+        totalCount += menuCount;
+
+        if (totalCount > Order.MAX_COUNT) {
+            return false;
+        }
+        return true;
+    }
+
+
 }
