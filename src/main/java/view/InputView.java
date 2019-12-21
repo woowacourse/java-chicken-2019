fd984168cb83d490b679833b49e5f5773f14b2f7
KSKIM <kwangsoonkim@outlook.com>
2019-12-21 17:41:44 +0900

    refactor: 하드코딩된 문자열 상수화

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,19 +7,28 @@ import domain.PosMenuType;
 import domain.Table;
 
 public class InputView {
+    private static final String MENU_SELECT_GUIDE_MESSAGE =
+            "## 메인화면\n1: 주문등록\n2: 결제하기\n3: 프로그램 종료\n";
+    private static final String TRY_SELECT_GUIDE_MESSAGE = "## 원하시는 기능을 선택하세요.";
+    private static final String INVALID_INPUT_MESSAGE = "## 잘못된 입력입니다. 다시 입력해주세요";
+    private static final String CREDIT_OR_CASH_GUIDE_MESSAGE = "## 신용카드는 1번, 현금은 2번";
+    private static final String SELECT_TABLE_GUIDE_MESSAGE = "## 주문할 테이블을 선택하세요.";
+    private static final String ADD_MENU_GUIDE_MESSAGE = "## 등록할 메뉴를 선택하세요.";
+    private static final String MENU_PURCHASE_COUNT_GUIDE_MESSAGE = "## 메뉴의 수량을 입력하세요.";
+
     private static final Scanner scanner = new Scanner(System.in);
 
 	// 묵시적 생성자 방지
     private InputView() {}
 
     public static PosMenuType inputPosMenu() {
-        System.out.println("## 메인화면\n1: 주문등록\n2: 결제하기\n3: 프로그램 종료\n");
+        System.out.println(MENU_SELECT_GUIDE_MESSAGE);
         try {
-            System.out.println("## 원하시는 기능을 선택하세요.");
+            System.out.println(TRY_SELECT_GUIDE_MESSAGE);
             String input = scanner.nextLine();
             return PosMenuType.of(input);
         } catch (IllegalArgumentException e) {
-            System.out.println("## 잘못된 입력입니다. 다시 입력해주세요");
+            System.out.println(INVALID_INPUT_MESSAGE);
             return inputPosMenu();
         }
     }
@@ -27,44 +36,44 @@ public class InputView {
     public static PaymentType inputPaymentType(Table table) {
         System.out.println("## " + table.getNumber() + "번 테이블의 결제를 진행합니다.");
         try {
-            System.out.println("## 신용카드는 1번, 현금은 2번");
+            System.out.println(CREDIT_OR_CASH_GUIDE_MESSAGE);
             String input = scanner.nextLine();
             return PaymentType.of(input);
         } catch (IllegalArgumentException e) {
-            System.out.println("## 잘못된 입력입니다. 다시 입력해주세요");
+            System.out.println(INVALID_INPUT_MESSAGE);
             return inputPaymentType(table);
         }
     }
 
     public static int inputTableNumber() {
         try {
-            System.out.println("## 주문할 테이블을 선택하세요.");
+            System.out.println(SELECT_TABLE_GUIDE_MESSAGE);
             String input = scanner.nextLine();
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            System.out.println("## 잘못된 입력입니다. 다시 입력해주세요");
+            System.out.println(INVALID_INPUT_MESSAGE);
             return inputTableNumber();
         }
     }
 
     public static int inputMenuNumber() {
         try {
-            System.out.println("## 등록할 메뉴를 선택하세요.");
+            System.out.println(ADD_MENU_GUIDE_MESSAGE);
             String input = scanner.nextLine();
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            System.out.println("## 잘못된 입력입니다. 다시 입력해주세요");
+            System.out.println(INVALID_INPUT_MESSAGE);
             return inputMenuNumber();
         }
     }
 
     public static int inputPurchaseCount() {
         try {
-            System.out.println("## 메뉴의 수량을 입력하세요.");
+            System.out.println(MENU_PURCHASE_COUNT_GUIDE_MESSAGE);
             String input = scanner.nextLine();
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            System.out.println("## 잘못된 입력입니다. 다시 입력해주세요");
+            System.out.println(INVALID_INPUT_MESSAGE);
             return inputPurchaseCount();
         }
     }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,39 +9,39 @@ import java.util.Map;
 
 public class OutputView {
 	private static final String TOP_LINE = "┌ ─ ┐";
-	private static final String TABLE_FORMAT = "|  %s   |";
+	private static final String TABLE_FORMAT = "|  %s  |";
 	private static final String BOTTOM_LINE = "└ ─ ┘";
 	private static final String BOTTOM_CASH_LINE = "└ $ ┘";
+	private static final String END_POS_MESSAGE = "## 포스기를 종료합니다.";
+	private static final String TABLE_LIST_GUIDE_MESSAGE = "## 테이블 목록";
+	private static final String ORDER_COUNT_EXCEED_MESSAGE =
+			"## 최대 주문 제한 개수를 초과하여 구매할 수 없습니다.";
+	private static final String NOT_HAS_UNPAID_TABLE_MESSAGE = "## 결제할 테이블이 없습니다.";
+	private static final String ORDER_LIST_GUIDE_MESSAGE = "## 주문 내역";
+	private static final String MENU_COUNT_PRICE_GUIDE_MESSAGE = "메뉴 수량 금액";
+	private static final String FINAL_PAYMENT_MESSAGE = "## 최종 결제할 금액";
 
 	// 묵시적 생성자 방지
 	private OutputView() {}
 
 	public static void printEndPos() {
-		System.out.println("## 포스기를 종료합니다.");
+		System.out.println(END_POS_MESSAGE);
 	}
 
 	public static void printTables(final List<Table> tables, Map<Table, List<Order>> tableOrders) {
-		System.out.println("## 테이블 목록");
+		System.out.println(TABLE_LIST_GUIDE_MESSAGE);
 		final int size = tables.size();
 		printLine(TOP_LINE, size);
 		printTableNumbers(tables);
 		printTableBottomLine(tables, tableOrders);
 	}
 
-	public static void printTables(final List<Table> tables) {
-		System.out.println("## 테이블 목록");
-		final int size = tables.size();
-		printLine(TOP_LINE, size);
-		printTableNumbers(tables);
-		printLine(BOTTOM_LINE, size);
-	}
-
 	public static void printOrderCountExceed() {
-		System.out.println("## 최대 주문 제한 개수를 초과하여 구매할 수 없습니다.");
+		System.out.println(ORDER_COUNT_EXCEED_MESSAGE);
 	}
 
 	public static void printNotHasUnpaidTable() {
-		System.out.println("## 결제할 테이블이 없습니다.");
+		System.out.println(NOT_HAS_UNPAID_TABLE_MESSAGE);
 	}
 
 	public static void printMenus(final List<Menu> menus) {
@@ -81,8 +81,8 @@ public class OutputView {
 	}
 
 	public static void printOrders(List<Order> orders) {
-        System.out.println("## 주문 내역");
-        System.out.println("메뉴 수량 금액");
+        System.out.println(ORDER_LIST_GUIDE_MESSAGE);
+        System.out.println(MENU_COUNT_PRICE_GUIDE_MESSAGE);
 		for (Order order: orders) {
             printOrder(order);
 		}
@@ -94,7 +94,7 @@ public class OutputView {
 	}
 
 	public static void printPaymentPrice(int price) {
-		System.out.println("## 최종 결제할 금액");
+		System.out.println(FINAL_PAYMENT_MESSAGE);
 		System.out.println(price);
 	}
 }
