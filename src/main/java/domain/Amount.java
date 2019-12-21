0c473fe33981d3d27aab6990635686c706124487
lalize <printf@kakao.com>
2019-12-21 16:59:44 +0900

    refactor: 예외 메시지 띄어쓰기 수정

--- src/main/java/domain/Amount.java
+++ src/main/java/domain/Amount.java
@@ -3,7 +3,7 @@ package domain;
 public class Amount {
 	private static final int MIN = 1;
 	private static final int MAX = 99;
-	private static final String AMOUNT_ERROR = "수량은 1 이상 99이하만 가능합니다.";
+	private static final String AMOUNT_ERROR = "수량은 1 이상 99 이하만 가능합니다.";
 
 	private final int amount;
 
