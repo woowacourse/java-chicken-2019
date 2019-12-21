ff8692effdb20f319470775d16b274584ae6e9d2
MOON JINJU <mjj920930@gmail.com>
2019-12-21 13:34:48 +0900

    feat: 메인화면에서 원하는 기능의 번호를 입력하는 기능, 숫자가 아닐 경우 예외처리 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,4 +9,14 @@ public class InputView {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
     }
+
+    public static int inputMainFunctionNumber() {
+        try {
+            System.out.println("## 원하는 기능을 선택하세요.");
+            return scanner.nextInt();
+        } catch (NumberFormatException e) {
+            System.out.println("숫자만 입력해주세요.");
+            return inputMainFunctionNumber();
+        }
+    }
 }
