52fa7de63c081ef302c25f5c17cc365ef1e22d2d
jaeju.jang <jjj0611@gmail.com>
2019-12-21 17:42:39 +0900

    add exception message about count

--- src/main/java/exception/InvalidInputException.java
+++ src/main/java/exception/InvalidInputException.java
@@ -6,6 +6,7 @@ public class InvalidInputException extends IllegalArgumentException {
     public static final String WRONG_MAIN_MENU_NUMBER_EXCEPTION = "메인 메뉴 번호를 잘못 선택하셨습니다.";
     public static final String WRONG_MENU_NUMBER_EXCEPTION = "존재하지 않는 메뉴 번호를 입력하셨습니다.";
     public static final String WRONG_TABLE_NUMBER_EXCEPTION = "존재하지 않는 테이블 번호를 입력하셨습니다.";
+    public static final String WRONG_COUNT_EXCEPTION = "수량은 0이하가 될 수 없습니다.";
 
     public InvalidInputException() {
         super();
