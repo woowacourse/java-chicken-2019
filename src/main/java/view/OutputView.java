f7eb8991ed90d758d8e528e4234ceb2599aa332e
Eun Seok <jeseok95@gmail.com>
2019-12-21 14:02:57 +0900

    [feat] 기능목록 기능구현
    주문등록, 결제하기, 프로그램종료을 보여주는 기능구현하였습니다.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -37,4 +37,14 @@ public class OutputView {
         }
         System.out.println();
     }
+
+    /**
+     * 프로그램 시작시 어떤 기능을 할지 선택지를 보여주는 메소드입니다.
+     */
+    public static void printMain(){
+        System.out.println("## 메인화면");
+        System.out.println("1 - 주문등록");
+        System.out.println("2 - 결제하기");
+        System.out.println("3 - 프로그램종료");
+    }
 }
