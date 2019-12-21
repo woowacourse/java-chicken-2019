425cdf6f0eca3e01003fbe36c7912ac6dc552052
gunhee lee <dlrjsgml42@naver.com>
2019-12-21 15:09:47 +0900

    [InputView] add exception about type mismatch 입력 중 타입이 미스매치되는 경우에 대한 예외처리를 여기서 담당한다. 만약 잘못된 타입이 들어오면 재귀적으로 자신을 반환한다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,17 +1,45 @@
 package view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
+    /**
+     * inputTableNumber는 테이블 번호를 입력받는 메서드이다.
+     * @return 테이블 번호를 반환한다.
+     * @exception InputMismatchException 잘못된 타입의 입력값이 들어오면 예외처리한다.
+     */
     public static int inputTableNumber() {
+        int inputNumber;
         System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
+        try {
+            inputNumber = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("잘못된 타입을 입력하셨습니다. 다시 입력해주세요.");
+            scanner = new Scanner(System.in);
+            return inputTableNumber();
+        }
+        return inputNumber;
     }
+
+    /**
+     * inputTableNumber는 주문/결제/종료에 대한 명령 번호(todoNumber)를 입력받는 메서드이다.
+     *
+     * @return 명령 번호를 반환한다.
+     * @exception InputMismatchException 잘못된 타입의 입력값이 들어오면 예외처리한다.
+     */
     public static int inputTodoNumber() {
-        System.out.println("## 메인화면\n1 - 주문등록\n2 - 결제하기\n3 - 프로그램 종료\n");
+        int inputNumber;
         System.out.println("## 원하는 기능을 선택하세요.");
-        return scanner.nextInt();
+        try {
+            inputNumber = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("잘못된 타입을 입력하셨습니다. 다시 입력해주세요.");
+            scanner = new Scanner(System.in);
+            return inputTodoNumber();
+        }
+        return inputNumber;
     }
 }
