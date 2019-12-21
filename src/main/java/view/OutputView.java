edb89ca1cc2a328ac28435d78f0d45aa301ef766
Unknown <unknown@example.com>
2019-12-21 13:33:25 +0900

    [ADD] 기능목록1 작성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -37,4 +37,11 @@ public class OutputView {
         }
         System.out.println();
     }
+
+    public static void printInitialMenu() {
+        System.out.println("## - 메인화면");
+        System.out.println("1 - 주문등록");
+        System.out.println("2 - 결제하기");
+        System.out.println("3 - 프로그램 종료");
+    }
 }
