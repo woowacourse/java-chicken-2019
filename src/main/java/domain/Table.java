240f0bc06bd531ec10eb5d9e4d38a0bb1d355ab4
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-21 16:59:59 +0900

    feat: Add map for ordered menus for each table
    HashMap orderedMap has menu number for key.
    and it has ordered quantaty for value.

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,14 +1,44 @@
 package domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Table {
-    private final int number;
+	private final int number;
 
-    public Table(final int number) {
-        this.number = number;
-    }
+	public Table(final int number) {
+		this.number = number;
+	}
 
-    @Override
-    public String toString() {
-        return Integer.toString(number);
-    }
+	@Override
+	public String toString() {
+		return Integer.toString(number);
+	}
+
+	private HashMap<Integer, Integer> orderMap;
+
+	private Map<Integer, Integer> getOrderMap() {
+		if (orderMap == null) {
+			orderMap = new HashMap<>();
+			return orderMap;
+		}
+		return orderMap;
+	}
+
+	public int getOrderQuantity(int menuChoice) {
+		Integer value = getOrderMap().get(menuChoice);
+		if (value == null) {
+			return 0;
+		}
+		return value;
+	}
+
+	public void putOrder(int menuChoice, int menuQuantity) {
+		Integer value = getOrderMap().get(menuChoice);
+		if (value == null) {
+			getOrderMap().put(menuChoice, menuQuantity);
+			return;
+		}
+		getOrderMap().put(menuChoice, value + menuQuantity);
+	}
 }
