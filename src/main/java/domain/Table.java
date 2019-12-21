4b988061c7d982d33390b8bae554a288e4e2a98b
lalize <printf@kakao.com>
2019-12-21 14:40:11 +0900

    feat: 테이블의 주문을 추가하는 기능 추가

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,7 +1,11 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Table {
     private final int number;
+    private final List<Order> orders = new ArrayList<>();
 
     public Table(final int number) {
         this.number = number;
@@ -14,5 +18,9 @@ public class Table {
 
     public boolean isNumber(int number) {
         return this.number == number;
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order);
     }
 }
