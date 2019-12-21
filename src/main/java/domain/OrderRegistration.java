a5865393fcdda56ddb7d94db4f798be580044893
김예림 <kiel0103@naver.com>
2019-12-21 15:18:01 +0900

    형식: 메서드 이름 오타수정

--- src/main/java/domain/OrderRegistration.java
+++ src/main/java/domain/OrderRegistration.java
@@ -1,7 +1,17 @@
 package domain;
 
-public class OrderRegistration {
-    public void resgistOrder() {
+import java.util.List;
+import view.InputView;
+import view.OutputView;
 
+public class OrderRegistration {
+    public void registerOrder() {
+        final List<Table> tables = TableRepository.tables();
+        OutputView.printTables(tables);
+
+        final int tableNumber = InputView.inputTableNumber();
+
+        final List<Menu> menus = MenuRepository.menus();
+        OutputView.printMenus(menus);
     }
 }
