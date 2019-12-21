91705d8c735ea36b117fdcbad646c83c7b540aca
plzprayme <dbfpzk142@gmail.com>
2019-12-21 14:50:42 +0900

    예외처리: 테이블 번호, 메뉴, 수량, 메인메뉴 입력할 때 정수인지 확인하기

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,30 +1,45 @@
 package view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static int inputMainMenu() {
         System.out.println("## 메인화면");
         System.out.println("1 - 주문등록");
         System.out.println("2 - 결제하기");
         System.out.println("3 - 프로그램 종료");
-        return scanner.nextInt();
+        return isInt();
     }
 
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
+        return isInt();
     }
 
     public static int inputMenuNumber() {
         System.out.println("## 등록할 메뉴를 선택하세요.");
-        return scanner.nextInt();
+        return isInt();
     }
 
     public static int inputHowManyMenu() {
         System.out.println("## 메뉴의 수량을 입력하세요.");
-        return scanner.nextInt();
+        return isInt();
     }
+
+    private static int isInt() {
+        while (true) {
+
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                scanner = new Scanner(System.in);
+                System.out.println("정수만 입력할 수 있습니다!");
+            }
+
+        }
+    }
+
 }
