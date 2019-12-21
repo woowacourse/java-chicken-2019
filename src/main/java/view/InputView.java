6e2d4895d0b459ce74f4bc39cdc7ac7b8d0bff05
hongbin <fusis1@naver.com>
2019-12-21 13:53:52 +0900

    [Add] 유저에 입력을 받을 수 있는 함수 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,7 +6,28 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
+        System.out.println("## 테이블을 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputMenuNumber() {
+        System.out.println("## 등록할 메뉴를 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputMenuQuantityNumber() {
+        System.out.println("## 메뉴의 수량을 입력하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputCardOrCash(int tableNumber) {
+        System.out.println("## " + tableNumber + "번 테이블의 결제를 진행합니다.");
+        System.out.println("## 신용카드는 1번, 현금은 2번");
+        return scanner.nextInt();
+    }
+
+    public static int inputMainFunction() {
+        System.out.println("## 원하는 기능을 선택하세요.");
         return scanner.nextInt();
     }
 }
