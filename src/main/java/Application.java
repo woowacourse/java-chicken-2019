a08d96337a4fc03bb6089e5b28bf6e0f25dc8a99
HWooKim97 <rgd5454@naver.com>
2019-12-21 14:48:53 +0900

    Add method run(), menu(), order(), pay()

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,14 +8,46 @@ import view.OutputView;
 import java.util.List;
 
 public class Application {
-    // TODO 구현 진행
-    public static void main(String[] args) {
-        final List<Table> tables = TableRepository.tables();
+    final List<Table> tables = TableRepository.tables();
+    final List<Menu> menus = MenuRepository.menus();
+
+    public void run() {
+        OutputView.printMain();
+        menu(InputView.inputMainNumber());
+    }
+
+    private void menu(int selection) {
+        if (selection == InputView.ORDER) {
+            order();
+            return;
+        }
+
+        if (selection == InputView.PAY) {
+            pay();
+            return;
+        }
+
+        if (selection == InputView.CLOSE)
+            System.exit(0);
+    }
+
+    private void order() {
         OutputView.printTables(tables);
+        int tableNumber = InputView.inputTableNumber();
 
-        final int tableNumber = InputView.inputTableNumber();
-
-        final List<Menu> menus = MenuRepository.menus();
         OutputView.printMenus(menus);
+        int menuNumber = InputView.inputMenuNumber();
+    }
+
+    private void pay() {
+
+    }
+
+    public static void main(String[] args) {
+        Application app = new Application();
+        while(true) {
+            app.run();
+            System.out.println();
+        }
     }
 }
