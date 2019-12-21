2fee4ff6bf6cd53f6af8340034be439b61be68ff
hyochan-Kim <kom1230@hanyang.ac.kr>
2019-12-21 17:16:59 +0900

    [ADD] 결제 금액을 담당하는 Payment 클래스 추가

---
+++ src/main/java/domain/Payment.java
@@ -0,0 +1,24 @@
+package domain;
+
+public class Payment {
+    private static final int PAYMENT_MIN = 0;
+    private static final int AMOUNT_MIN = 0;
+    private static final int TEN = 10;
+    private static final int CASH = 2;
+    private static final int DISCOUNT = 10000;
+    private static final double CASH_DISCOUNT_RATIO = 0.95;
+
+    private final int paymentAmount;
+    private final int chickenAmount;
+
+    public Payment(int paymentAmount, int chickenAmount) {
+        if (paymentAmount < PAYMENT_MIN) {
+            throw new IllegalArgumentException("주문 가격은 " + PAYMENT_MIN + "이상이어야 합니다.");
+        }
+        if (chickenAmount < AMOUNT_MIN) {
+            throw new IllegalArgumentException("주문 수량은 " + AMOUNT_MIN + "이상이어야 합니다.");
+        }
+        this.paymentAmount = paymentAmount;
+        this.chickenAmount = chickenAmount;
+    }
+}
