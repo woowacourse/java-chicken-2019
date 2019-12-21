b44b0a8878c0e1915fcc9527ee556c1d6fa29f9e
김예림 <kiel0103@naver.com>
2019-12-21 17:41:34 +0900

    기능: 할인 액수 계산하기 기능 구현

--- src/main/java/domain/DiscountCalculator.java
+++ src/main/java/domain/DiscountCalculator.java
@@ -2,4 +2,10 @@ package domain;
 
 public class DiscountCalculator {
 
+    /**
+     * @return 할인 액수
+     */
+    public static int getDiscountAmount(int notDiscountedAmount, int chickenNumber, boolean isCash) {
+        return 0;
+    }
 }
