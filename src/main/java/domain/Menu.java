694cd718551219037e4363ae7860237ccb23e841
woonjang <woonjangahn@gmail.com>
2019-12-21 16:18:30 +0900

    기능: 메뉴 객체 getter들 필요에 따라 추가

--- src/main/java/domain/Menu.java
+++ src/main/java/domain/Menu.java
@@ -17,6 +17,14 @@ public class Menu {
         return number;
     }
 
+    public int getPrice() {
+        return price;
+    }
+
+    public boolean isChicken() {
+        return category.equals(Category.CHICKEN);
+    }
+
     @Override
     public String toString() {
         return category + " " + number + " - " + name + " : " + price + "원";
