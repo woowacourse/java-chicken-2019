a2ec2aa7d265869168ee36a79f5e98f7642ace6a
taeheon <dqrd123@gmail.com>
2019-12-21 13:57:16 -0800

    실수로 입력 구현에 메인화면 출력을 포함해서 수정하였다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,13 +5,6 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void mainDisplay(){
-        System.out.println("\n##메인화면");
-        System.out.println("1 - 주문등록");
-        System.out.println("2 - 결제하기");
-        System.out.println("3 - 프로그램 종료");
-    }
-
     public static int inputFunctionNumber(){
         System.out.println("\n## 원하는 기능을 선택하세요.");
         return scanner.nextInt();
