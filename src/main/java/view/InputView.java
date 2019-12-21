f7f6045e0480b93a3537d182596057bfc6dfc672
SMJin <sstaw@naver.com>
2019-12-21 14:04:49 +0900

    [InputView Class] inputPosMenu 메소드 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,21 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
+    public static int inputPosMenu() {
+        println("## 메인화면\n" +
+                "1 - 주문등록\n" +
+                "2 - 결제하기\n" +
+                "3 - 프로그램 종료");
+        println("## 원하는 기능을 선택하세요.");
         return scanner.nextInt();
+    }
+
+    public static int inputTableNumber() {
+        println("## 주문할 테이블을 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    private static void println(String message) {
+        System.out.println("\n" + message);
     }
 }
