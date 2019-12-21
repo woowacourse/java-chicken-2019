6049e8ba7c7f72cc635ad00095cca32c76d1bdf7
woonjang <woonjangahn@gmail.com>
2019-12-21 16:56:55 +0900

    기능: 출력 형태 제시된 예시와 비슷하도록 줄 사이 공백 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,27 +9,37 @@ public class InputView {
     public static int inputMain() {
         OutputView.printFeatures();
         System.out.println("## 원하는 기능을 선택하세요.");
-        return scanner.nextInt();
+        int input = scanner.nextInt();
+        OutputView.printBlankLine();
+        return input;
     }
 
     public static int inputTableNumber() {
         System.out.println("## 테이블을 선택하세요.");
-        return scanner.nextInt();
+        int input = scanner.nextInt();
+        OutputView.printBlankLine();
+        return input;
     }
 
     public static int inputMenuNumber() {
         System.out.println("## 등록할 메뉴를 선택하세요.");
-        return scanner.nextInt();
+        int input = scanner.nextInt();
+        OutputView.printBlankLine();
+        return input;
     }
 
     public static int inputMenuQuantity() {
         System.out.println("## 메뉴의 수량을 입력하세요.");
-        return scanner.nextInt();
+        int input = scanner.nextInt();
+        OutputView.printBlankLine();
+        return input;
     }
 
     public static int inputPayMethod(int tableNumber) {
         System.out.println("## " + tableNumber + "번 테이블의 결제를 진행합니다.\n"
             + "## 신용 카드는 1번, 현금은 2번");
-        return scanner.nextInt();
+        int input = scanner.nextInt();
+        OutputView.printBlankLine();
+        return input;
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,12 +10,14 @@ public class OutputView {
     private static final String TOP_LINE = "┌ ─ ┐";
     private static final String TABLE_FORMAT = "| %s |";
     private static final String BOTTOM_LINE = "└ ─ ┘";
+    private static final String BOTTOM_ALTERNATIVE = "└ ₩ ┘";
 
     public static void printFeatures() {
         System.out.println("## 메인화면\n"
             + "1 - 주문등록\n"
             + "2 - 결제하기\n"
             + "3 - 프로그램 종료");
+        OutputView.printBlankLine();
     }
 
     public static void printTables(final List<Table> tables) {
@@ -24,14 +26,15 @@ public class OutputView {
         printLine(TOP_LINE, size);
         printTableNumbers(tables);
         printLine(BOTTOM_LINE, size);
-
         // TODO: 주문이 존재하는 경우 BOTTOM_LINE 을 "└ ₩ ┘" 으로 교체
+        OutputView.printBlankLine();
     }
 
     public static void printMenus(final List<Menu> menus) {
         for (final Menu menu : menus) {
             System.out.println(menu);
         }
+        printBlankLine();
     }
 
     private static void printLine(final String line, final int count) {
@@ -51,9 +54,15 @@ public class OutputView {
     public static void printOrders(Order order) {
         StringBuilder sb = new StringBuilder("메뉴 수량 금액\n").append(order);
         System.out.println(sb.toString());
+        printBlankLine();
     }
 
     public static void printTotal(double amount) {
         System.out.println("## 최종 결제할 금액\n" + (int)amount + "원");
+        printBlankLine();
+    }
+
+    public static void printBlankLine() {
+        System.out.println();
     }
 }
