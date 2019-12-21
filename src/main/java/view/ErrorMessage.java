05f4cc6872daefb9246f32bca9b6fb113d860cbb
jiwon_lala <s26788761@naver.com>
2019-12-21 13:55:30 +0900

    feat : Create ErrorMessage Class
    잘못된 입력에 해당하는 에러메시지를 출력할 때 사용할 ErrorMessage 클래스 작성

---
+++ src/main/java/view/ErrorMessage.java
@@ -0,0 +1,9 @@
+package view;
+
+public class ErrorMessage {
+
+    public static final String CONTAINS_CHAR = "입력에 문자가 포함되었습니다. 다시 입력해주세요.";
+    public static final String IS_MINUS = "입력한 수가 음수입니다. 다시 입력해주세요.";
+    public static final String IS_ZERO = "입력한 수가 0 입니다. 다시 입력해주세요.";
+    public static final String THAT_INPUT_IS_NOT_EXIST = "입력에 해당하는 항목이 없습니다. 다시 입력해주세요.";
+}
