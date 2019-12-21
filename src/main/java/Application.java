b0287532fc9966a81c4ac995a102e1a153e6fdf8
aegis1920 <aegis1920@gmail.com>
2019-12-21 13:42:43 +0900

    [feat] add : 메인화면 출력

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,11 +9,17 @@ import java.util.List;
 
 public class Application {
     // TODO 구현 진행
+
     public static void main(String[] args) {
         final List<Table> tables = TableRepository.tables();
+
+        OutputView.printMain();
+
         OutputView.printTables(tables);
 
         final int tableNumber = InputView.inputTableNumber();
+
+
 
         final List<Menu> menus = MenuRepository.menus();
         OutputView.printMenus(menus);
