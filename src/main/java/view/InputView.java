f24f771b005085a4b0de0df8aced53a7ec2854ce
tiroring09 <tiroring09@gmail.com>
2019-12-21 14:00:39 +0900

    [feat] 메인화면에 기능설명 출력

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,10 +3,14 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+	private static Scanner scanner = new Scanner(System.in);
 
-    public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
-    }
+	public static int inputTableNumber() {
+		System.out.println("## 주문할 테이블을 선택하세요.");
+		return scanner.nextInt();
+	}
+
+	public static void mainView() {
+		System.out.println("## 메인화면\n1 - 주문등록\n2 - 결제하기\n3 - 프로그램 종료\n");
+	}
 }
