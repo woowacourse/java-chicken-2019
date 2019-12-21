ddf3f4d53a18bfd868b80cb8e98b0d33b6911fe3
김예림 <kiel0103@naver.com>
2019-12-21 15:13:05 +0900

    기능: 사용중인 테이블과 아닌 테이블을 구별하여 출력하기 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,7 +9,8 @@ import java.util.List;
 public class OutputView {
     private static final String TOP_LINE = "┌ ─ ┐";
     private static final String TABLE_FORMAT = "| %s |";
-    private static final String BOTTOM_LINE = "└ ─ ┘";
+    private static final String NOT_IN_USE_TABLE_BOTTOM_LINE = "└ ─ ┘";
+    private static final String IN_USE_TABLE_BOTTOM_LINE = "└ $ ┘";
     private static final String PROVIDING_FUNCTION_FORMAT = "%d - %s";
 
     public static void printProvidingFunctions(final List<ProvidingFunction> providingFunctions) {
@@ -29,15 +30,38 @@ public class OutputView {
     public static void printTables(final List<Table> tables) {
         System.out.println("## 테이블 목록");
         final int size = tables.size();
-        printLine(TOP_LINE, size);
+        printTopLine(size);
         printTableNumbers(tables);
-        printLine(BOTTOM_LINE, size);
+        printBottomLine(tables);
     }
 
     public static void printMenus(final List<Menu> menus) {
         for (final Menu menu : menus) {
             System.out.println(menu);
         }
+    }
+
+    private static void printTopLine(final int size) {
+        printLine(TOP_LINE, size);
+    }
+
+    private static void printBottomLine(final List<Table> tables) {
+
+        for (Table table : tables) {
+            printOneTableBottom(table.isInUse());
+        }
+
+        System.out.println();
+    }
+
+    private static void printOneTableBottom(final boolean isInUse) {
+
+        if (isInUse) {
+            System.out.print(IN_USE_TABLE_BOTTOM_LINE);
+            return;
+        }
+
+        System.out.print(NOT_IN_USE_TABLE_BOTTOM_LINE);
     }
 
     private static void printLine(final String line, final int count) {
