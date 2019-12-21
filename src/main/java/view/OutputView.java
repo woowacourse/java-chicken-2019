455e0e4a9802716086226684b05354040e3e64fc
sssssseungmi <tmdal9516@naver.com>
2019-12-21 17:16:35 +0900

    Refactor printMainMenu '메인메뉴' to '메인화면'

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -53,7 +53,7 @@ public class OutputView {
 	}
 
 	public static void printMainMenu() {
-		System.out.println("## 메인메뉴");
+		System.out.println("## 메인화면");
 		System.out.println("1-주문등록");
 		System.out.println("2-결제하기");
 		System.out.println("3-프로그램 종료");
