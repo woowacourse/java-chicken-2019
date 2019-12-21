12697ab24ed903b70bd3bdbe7cb7c23d7c347e95
chanin <pci2676@gmail.com>
2019-12-21 14:01:08 +0900

    refactor : change name Menues -> Orders
     - Menues 클래스 이름 변경

--- src/main/java/domain/Orders.java
+++ src/main/java/domain/Orders.java
@@ -2,21 +2,21 @@ package domain;
 
 import java.util.List;
 
-public class Menues {
-    private final List<Menu> menues;
+public class Orders {
+    private final List<Menu> orders;
 
-    public Menues(List<Menu> menues) {
-        this.menues = menues;
+    public Orders(List<Menu> orders) {
+        this.orders = orders;
     }
 
     public int chickenSize() {
-        return (int) menues.stream()
+        return (int) orders.stream()
                 .filter(Menu::isChicken)
                 .count();
     }
 
     public int getPrice() {
-        return menues.stream()
+        return orders.stream()
                 .map(Menu::getPrice)
                 .reduce(0, Integer::sum);
     }
--- src/test/java/domain/OrdersTest.java
+++ src/test/java/domain/OrdersTest.java
@@ -7,16 +7,16 @@ import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class MenuesTest {
+class OrdersTest {
 
     @DisplayName("주문 항목이 가지고 있는 치킨 메뉴의 갯수")
     @Test
     void chickenSize() {
         Menu chicken = aMenu(Category.CHICKEN, 100);
         Menu beverage = aMenu(Category.BEVERAGE, 100);
-        Menues menues = new Menues(Arrays.asList(chicken, beverage));
+        Orders orders = new Orders(Arrays.asList(chicken, beverage));
 
-        assertThat(menues.chickenSize()).isEqualTo(1);
+        assertThat(orders.chickenSize()).isEqualTo(1);
     }
 
     @DisplayName("주문 항목의 가격 총합")
@@ -24,9 +24,9 @@ class MenuesTest {
     void getPrice() {
         Menu chicken = aMenu(Category.CHICKEN, 100);
         Menu beverage = aMenu(Category.BEVERAGE, 200);
-        Menues menues = new Menues(Arrays.asList(chicken, beverage));
+        Orders orders = new Orders(Arrays.asList(chicken, beverage));
 
-        assertThat(menues.getPrice()).isEqualTo(300);
+        assertThat(orders.getPrice()).isEqualTo(300);
     }
 
     public Menu aMenu(Category category, int price) {
