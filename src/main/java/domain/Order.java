4f5befbe0d6aa161df4699eabad7a571a9984eb0
pandahun <gnsgnsek@gmail.com>
2019-12-21 15:52:32 +0900

    feature: Create Order.java
    Order객체 생성

---
+++ src/main/java/domain/Order.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Order {
+
+    final int number;
+    final int quantity;
+
+    public Order(int number, int quantity) {
+        this.number = number;
+        this.quantity = quantity;
+    }
+
+}
