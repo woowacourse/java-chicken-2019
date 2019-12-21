b276e42090ac240e11807f66049217ccd658784b
김예림 <kiel0103@naver.com>
2019-12-21 16:33:22 +0900

    기능: 메뉴의 수량 입력받기 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -19,4 +19,9 @@ public class InputView {
         System.out.println("## 등록할 메뉴를 선택하세요.");
         return scanner.nextInt();
     }
+
+    public static int inputNumberOfMenu() {
+        System.out.println("## 메뉴의 수량을 입력하세요.");
+        return scanner.nextInt();
+    }
 }
