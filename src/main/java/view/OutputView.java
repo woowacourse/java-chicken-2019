2463efb931a2d94ba21730e681c6cedc44f8ea34
ordinCode <ysh1346@naver.com>
2019-12-21 13:32:57 +0900

    메인화면 출력 생성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -37,4 +37,11 @@ public class OutputView {
         }
         System.out.println();
     }
+
+    public static void printMainScreen(){
+        System.out.println("##메인 화면\n1 - 주문등\n2 - 결제하기\n3 - 프로그램 종료");
+
+    }
+
+
 }
