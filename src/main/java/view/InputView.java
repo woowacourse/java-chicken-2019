bea67f6ecb4f238b96b0cdff7079b759e8846bb5
LTTTTTE <10100aaaaa@gmail.com>
2019-12-21 13:35:40 +0900

    메인화면 에서 주문,결제,종료 입력 받는 기능 추가 - 유효한 입력인지 확인하는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,13 +1,28 @@
 package view;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 public class InputView {
+    private static final int ONE = 1;
+    private static final int TWO = 2;
+    private static final int THREE = 3;
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return inputAsNumber();
+    }
+
+    public static int inputMainMenu() {
+        OutputView.mainView();
+        return Optional.of(inputAsNumber())
+                .filter(InputView::isMainInputValid)
+                .orElseGet(InputView::inputMainMenu);
+    }
+
+    public static boolean isMainInputValid(int input) {
+        return input == ONE || input == TWO || input == THREE;
     }
 
     public static int inputAsNumber() {
