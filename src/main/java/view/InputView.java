94c775898781224b0b94a82b801cddcb8000f680
ordinCode <ysh1346@naver.com>
2019-12-21 14:02:26 +0900

    원하는 기능 입력 완성
    - 원하는 기능 입력시
      - [v] 숫자가 아닌 문자 입력
      - [v] 1,2,3 이외의 입력
    
    예외처리 완료

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,18 +1,24 @@
 package view;
 
+import Check.Check;
+
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static String inputTableNumber() {
+    public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
 
-        return scanner.next();
+        return scanner.nextInt();
     }
 
-    public String inputFunction(){
+    public int inputFunction() {
         System.out.println("원하는 기능을 입력하세요.");
-        return scanner.next();
+        String userInput = scanner.next();
+        while (new Check().isInputFunctionError(userInput)) {
+            userInput = scanner.next();
+        }
+        return Integer.parseInt(userInput);
     }
 }
