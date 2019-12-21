604d5d12a219be68f93f71eed2b79a4e56196d19
lxxjn0 <ljy3510@gmail.com>
2019-12-21 13:54:31 +0900

    feat : 사용자가 원하는 기능 입력을 받아오는 기능의 예외 처리
    1. 사용자가 원하는 기능의 입력이 숫자가 아닌 입력이 들어오는 경우를 예외 처리.
    2. 사용자가 원하는 기능의 입력이 1부터 3까지의 숫자가 아닌 경우를 예외처리.
    3. README.md에 구현해야 할 기능에 예외 처리 추가 및 개발 진행 계획 추가.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,6 +5,7 @@ import domain.TableRepository;
 import view.InputView;
 import view.OutputView;
 
+import java.util.InputMismatchException;
 import java.util.List;
 
 public class Application {
@@ -12,7 +13,7 @@ public class Application {
     public static void main(String[] args) {
         OutputView.printFunctions();
 
-        final int functionNumber = InputView.inputFunctionNumber();
+        final int functionNumber = getInputFunctionNumber();
 
         final List<Table> tables = TableRepository.tables();
         OutputView.printTables(tables);
@@ -21,5 +22,25 @@ public class Application {
 
         final List<Menu> menus = MenuRepository.menus();
         OutputView.printMenus(menus);
+    }
+
+    private static int getInputFunctionNumber() {
+        try {
+            final int functionNumber = InputView.inputFunctionNumber();
+            return isValid(functionNumber);
+        } catch (InputMismatchException e) {
+            System.out.println("1부터 3까지의 숫자만 입력 가능합니다.");
+            return getInputFunctionNumber();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getInputFunctionNumber();
+        }
+    }
+
+    private static int isValid(int functionNumber) {
+        if (functionNumber > 3 || functionNumber < 1) {
+            throw new IllegalArgumentException("입력한 기능 목록이 존재하지 않습니다. 1부터 3까지의 숫자만 입력 가능합니다.");
+        }
+        return functionNumber;
     }
 }
