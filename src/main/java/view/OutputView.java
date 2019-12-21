3ced353de2a8a2d76488a9299118013598ed895e
KSKIM <kwangsoonkim@outlook.com>
2019-12-21 17:19:52 +0900

    feat: 결제 총액을 출력하는 메소드 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,71 +8,93 @@ import java.util.List;
 import java.util.Map;
 
 public class OutputView {
-    private static final String TOP_LINE = "┌ ─ ┐";
-    private static final String TABLE_FORMAT = "|  %s   |";
-    private static final String BOTTOM_LINE = "└ ─ ┘";
-    private static final String BOTTOM_CASH_LINE = "└ $ ┘";
+	private static final String TOP_LINE = "┌ ─ ┐";
+	private static final String TABLE_FORMAT = "|  %s   |";
+	private static final String BOTTOM_LINE = "└ ─ ┘";
+	private static final String BOTTOM_CASH_LINE = "└ $ ┘";
 
-    // 묵시적 생성자 방지
-    private OutputView() {}
+	// 묵시적 생성자 방지
+	private OutputView() {}
 
-    public static void printEndPos() {
-        System.out.println("## 포스기를 종료합니다.");
-    }
+	public static void printEndPos() {
+		System.out.println("## 포스기를 종료합니다.");
+	}
 
-    public static void printTables(final List<Table> tables, Map<Table, List<Order>> tableOrders) {
-        System.out.println("## 테이블 목록");
-        final int size = tables.size();
-        printLine(TOP_LINE, size);
-        printTableNumbers(tables);
-        printTableBottomLine(tables, tableOrders);
-    }
+	public static void printTables(final List<Table> tables, Map<Table, List<Order>> tableOrders) {
+		System.out.println("## 테이블 목록");
+		final int size = tables.size();
+		printLine(TOP_LINE, size);
+		printTableNumbers(tables);
+		printTableBottomLine(tables, tableOrders);
+	}
 
-    public static void printTables(final List<Table> tables) {
-        System.out.println("## 테이블 목록");
-        final int size = tables.size();
-        printLine(TOP_LINE, size);
-        printTableNumbers(tables);
-        printLine(BOTTOM_LINE, size);
-    }
+	public static void printTables(final List<Table> tables) {
+		System.out.println("## 테이블 목록");
+		final int size = tables.size();
+		printLine(TOP_LINE, size);
+		printTableNumbers(tables);
+		printLine(BOTTOM_LINE, size);
+	}
 
-    public static void printOrderCountExceed() {
-        System.out.println("최대 주문 제한 개수를 초과하여 구매할 수 없습니다.");
-    }
+	public static void printOrderCountExceed() {
+		System.out.println("## 최대 주문 제한 개수를 초과하여 구매할 수 없습니다.");
+	}
 
-    public static void printMenus(final List<Menu> menus) {
-        for (final Menu menu : menus) {
-            System.out.println(menu);
-        }
-    }
+	public static void printNotHasUnpaidTable() {
+		System.out.println("## 결제할 테이블이 없습니다.");
+	}
 
-    private static void printLine(final String line, final int count) {
-        for (int index = 0; index < count; index++) {
-            System.out.print(line);
-        }
-        System.out.println();
-    }
+	public static void printMenus(final List<Menu> menus) {
+		for (final Menu menu : menus) {
+			System.out.println(menu);
+		}
+	}
 
-    private static void printTableNumbers(final List<Table> tables) {
-        for (final Table table : tables) {
-            System.out.printf(TABLE_FORMAT, table);
-        }
-        System.out.println();
-    }
+	private static void printLine(final String line, final int count) {
+		for (int index = 0; index < count; index++) {
+			System.out.print(line);
+		}
+		System.out.println();
+	}
 
-    private static void printTableBottomLine(final List<Table> tables, Map<Table, List<Order>> tableOrders) {
-        for (final Table table : tables) {
-            printTableBottomLine(tableOrders.get(table).size());
-        }
-        System.out.println();
-    }
+	private static void printTableNumbers(final List<Table> tables) {
+		for (final Table table : tables) {
+			System.out.printf(TABLE_FORMAT, table);
+		}
+		System.out.println();
+	}
 
-    private static void printTableBottomLine(int tableOrderCount) {
-        if (tableOrderCount == 0) {
-            System.out.print(BOTTOM_LINE);
-        }
-        if (tableOrderCount != 0) {
-            System.out.print(BOTTOM_CASH_LINE);
-        }
-    }
+	private static void printTableBottomLine(final List<Table> tables, Map<Table, List<Order>> tableOrders) {
+		for (final Table table : tables) {
+			printTableBottomLine(tableOrders.get(table).size());
+		}
+		System.out.println();
+	}
+
+	private static void printTableBottomLine(int tableOrderCount) {
+		if (tableOrderCount == 0) {
+			System.out.print(BOTTOM_LINE);
+		}
+		if (tableOrderCount != 0) {
+			System.out.print(BOTTOM_CASH_LINE);
+		}
+	}
+
+	public static void printOrders(List<Order> orders) {
+        System.out.println("## 주문 내역");
+        System.out.println("메뉴 수량 금액");
+		for (Order order: orders) {
+            printOrder(order);
+		}
+		System.out.println();
+	}
+
+	private static void printOrder(Order order) {
+        System.out.println(order.toPrintString());
+	}
+
+	public static void printPaymentPrice(int price) {
+		System.out.println("## 최종 결제할 금액");
+		System.out.println(price);
+	}
 }
