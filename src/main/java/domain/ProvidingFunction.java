65cfb4b7a88751468c762273744ce6745b27dd44
김예림 <kiel0103@naver.com>
2019-12-21 14:15:16 +0900

    기능: 제공 기능 객체 클래스 자바파일 생성 및 작성
    이 어플리케이션에서 제공하는 기능 하나하나에 대한 객체

---
+++ src/main/java/domain/ProvidingFunction.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class ProvidingFunction {
+    private int id;
+    private String description;
+
+    public ProvidingFunction(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+}
