f184ad5599627c4a1ea8d3c462905ae41c6c1475
zion830 <zion830@honglab.org>
2019-12-21 16:07:18 +0900

    feat: 주문, 수량 정보를 생성하는 클래스 추가

---
+++ src/main/java/domain/OrderCreator.java
@@ -0,0 +1,50 @@
+package domain;
+
+import util.InputValidator;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+
+/**
+ * 주방에 전달할 주문을 관리하는 역할의 클래스
+ */
+public class OrderCreator {
+    private List<Menu> menuList = MenuRepository.menus();
+
+    private int inputMenuNumber() {
+        String input = "";
+
+        do {
+            input = InputView.inputMenu();
+        } while (!InputValidator.isNumber(input));
+
+        return Integer.parseInt(input);
+    }
+
+    private int inputCount() {
+        String input = "";
+
+        do {
+            input = InputView.inputMenu();
+        } while (!InputValidator.isNumber(input));
+
+        return Integer.parseInt(input);
+    }
+
+    private Menu selectMenu() {
+        OutputView.printMenus(menuList);
+        int menuNumber = inputMenuNumber();
+
+        while (!MenuRepository.isExist(menuNumber)) {
+            OutputView.printNotExistMenu();
+            menuNumber = inputMenuNumber();
+        }
+
+        return MenuRepository.search(menuNumber);
+    }
+
+    public Order makeOrder() {
+        return new Order(selectMenu(), inputCount());
+    }
+}
