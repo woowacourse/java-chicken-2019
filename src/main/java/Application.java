db31bf6015aac8241f793b4a7e76937585d58d6e
KongsonPark <sonypark0204+wootech@gmail.com>
2019-12-21 14:25:46 +0900

    [feat] 주문 완료 후 메인화면으로 돌아가는 기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,24 +1,20 @@
-import domain.Menu;
-import domain.MenuRepository;
-import domain.Table;
-import domain.TableRepository;
 import order.Order;
 import view.InputView;
 import view.OutputView;
 
-import java.util.List;
-
 public class Application {
 	// TODO 구현 진행
 	public static void main(String[] args) {
-
-		OutputView.printMainScreen();
-		int process = InputView.inputMainProcess();
 		Order order = new Order();
 
-		if (process == 1) {
-			order.orderMenu();
-		}
+		do {
+			OutputView.printMainScreen();
+			int process = InputView.inputMainProcess();
+			if (process == 1) {
+				continue;
+			}
+			break;
+		} while (order.orderMenu());
 
 	}
 }
---
+++ src/main/java/order/Order.java
@@ -0,0 +1,29 @@
+package order;
+
+import java.util.List;
+
+import domain.Menu;
+import domain.MenuRepository;
+import domain.Table;
+import domain.TableRepository;
+import view.InputView;
+import view.OutputView;
+
+public class Order {
+
+	public boolean orderMenu() {
+
+		final List<Table> tables = TableRepository.tables();
+		OutputView.printTables(tables);
+
+		final int tableNumber = InputView.inputTableNumber();
+
+		final List<Menu> menus = MenuRepository.menus();
+		OutputView.printMenus(menus);
+
+		InputView.inputOrderMenu();
+		InputView.inputNumberOfMenu();
+
+		return true;
+	}
+}
