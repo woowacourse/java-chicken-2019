7a0b44578a8dd33cfd774418fcd02abe762f113b
junho <hwanghe159@naver.com>
2019-12-21 14:12:36 +0900

    전체적인 진행을 담당하는 ChickenHouse내부의 execute()생성, 구조 구축

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,33 +9,7 @@ import java.util.List;
 
 public class Application {
 	public static void main(String[] args) {
-		final List<Table> tables = TableRepository.tables();
-		while (true) {
-			OutputView.mainDisplay();
-			int command = InputView.inputTableNumber();
-			if (command == 1) {
-				order();
-			}
-			if (command == 2) {
-				pay();
-			}
-			if (command == 3) {
-				return;
-			}
-		}
+		ChickenHouse pos = new ChickenHouse();
+		pos.execute();
 	}
-
-	private static void order() {
-		OutputView.printTables(tables);
-
-		final int tableNumber = InputView.inputTableNumber();
-
-		final List<Menu> menus = MenuRepository.menus();
-		OutputView.printMenus(menus);
-	}
-
-	private static void pay() {
-
-	}
-
 }
--- src/main/java/ChickenHouse.java
+++ src/main/java/ChickenHouse.java
@@ -1,4 +1,42 @@
+import java.util.List;
+
+import domain.Menu;
+import domain.MenuRepository;
+import domain.Table;
+import domain.TableRepository;
+import view.InputView;
+import view.OutputView;
 
 public class ChickenHouse {
+	final static List<Table> tables = TableRepository.tables();
+
+	public void execute() {
+		while (true) {
+			OutputView.mainDisplay();
+			int command = InputView.inputTableNumber();
+			if (command == 1) {
+				order();
+			}
+			if (command == 2) {
+				pay();
+			}
+			if (command == 3) {
+				return;
+			}
+		}
+	}
+
+	private static void order() {
+		OutputView.printTables(tables);
+
+		final int tableNumber = InputView.inputTableNumber();
+
+		final List<Menu> menus = MenuRepository.menus();
+		OutputView.printMenus(menus);
+	}
+
+	private static void pay() {
+
+	}
 
 }
