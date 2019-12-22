a3a5bc761d79d90bf4d46a1efdfeb865301b978e
KongsonPark <sonypark0204+wootech@gmail.com>
2019-12-22 16:11:35 +0900

    [refactor] 결제 클래스에 숫자를 클래스 상수로 변경

--- src/main/java/pay/Pay.java
+++ src/main/java/pay/Pay.java
@@ -8,8 +8,10 @@ import view.InputView;
 public class Pay {
 	private static final double CASH_DISCOUNT = 0.95;
 	private static final int CHICKEN_DISCOUNT = 10000;
-	private static final double CREDIT_CARD = 1;
-	private static final double CASH = 2;
+	private static final int CHICKEN_DISCOUNT_UNIT = 10;
+	private static final int CREDIT_CARD = 1;
+	private static final int CASH = 2;
+	private static final int ZERO = 0;
 
 	public static void pay(List<OrderMenu> orderMenuList, int tableNumber) {
 		printOrderList(orderMenuList);
@@ -21,7 +23,7 @@ public class Pay {
 	}
 
 	private static double getPayment(int paymentType, int payMoney, int chickenDiscount) {
-		double payment = 0;
+		double payment = ZERO;
 
 		if (paymentType == CREDIT_CARD) {
 			payment = payMoney - chickenDiscount;
@@ -29,19 +31,19 @@ public class Pay {
 		if (paymentType == CASH) {
 			payment = (payMoney * CASH_DISCOUNT) - chickenDiscount;
 		}
-		if (payment < 0) {
-			return 0;
+		if (payment < ZERO) {
+			return ZERO;
 		}
 		return payment;
 	}
 
 	private static int getChickenDiscount(List<OrderMenu> orderMenuList) {
 		int chickenNumber = getNumberOfChickenMenu(orderMenuList);
-		int discountQuantity = chickenNumber / 10;
-		if (discountQuantity > 0) {
+		int discountQuantity = chickenNumber / CHICKEN_DISCOUNT_UNIT;
+		if (discountQuantity > ZERO) {
 			return discountQuantity * CHICKEN_DISCOUNT;
 		}
-		return 0;
+		return ZERO;
 	}
 
 	private static int getNumberOfChickenMenu(List<OrderMenu> orderMenuList) {
